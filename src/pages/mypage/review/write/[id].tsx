213fb93548f59be9ee7de0017fdf74359e838ebe
donghakim <donghakim@donghakimui-MacBookPro.local>
2021-12-21 14:02:47 +0900

    DEV-822 이미지 업로드 완성

--- src/pages/mypage/review/write/[id].tsx
+++ src/pages/mypage/review/write/[id].tsx
@@ -21,14 +21,39 @@ import StarRatingComponent from 'react-star-rating-component';
 import SVGIcon from '@utils/SVGIcon';
 import debounce from 'lodash-es/debounce';
 import BorderLine from '@components/Shared/BorderLine';
+import TextArea from '@components/Shared/TextArea';
+import TextInput from '@components/Shared/TextInput';
+import { getImageSize } from '@utils/getImageSize';
+
+interface IWriteMenuReviewObj {
+  dateId: number;
+  detailId: number;
+  imgFiles: string[];
+  deletedImgIds: string[];
+  content: string;
+  rating: number;
+}
 
 function WriteReviewPage({ id }: any) {
   const [isShow, setIsShow] = useState(false);
   const [item, setItem] = useState<any>({});
   const [rating, setRating] = useState<number>(5);
   const [numberOfReivewContent, setNumberOfReivewContent] = useState<number>(0);
-
+  const [previewImg, setPreviewImg] = useState<string[]>([]);
+  const [writeMenuReviewObj, setWriteMenuReviewObj] =
+    useState<IWriteMenuReviewObj>({
+      dateId: 0,
+      detailId: 0,
+      imgFiles: [],
+      deletedImgIds: [],
+      content: '',
+      rating: 5,
+    });
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  /* TODO: text area 1000 넘었을 때 */
+  /* TODO: blob 타입 정의 */
+  /* TODO: 사이즈 체크 및 사진 올리는 hooks */
 
   useEffect(() => {
     getItemForReview();
@@ -61,6 +86,82 @@ function WriteReviewPage({ id }: any) {
       setNumberOfReivewContent(textAreaRef.current?.value.length);
     }
   }, 50);
+
+  const onChangeFileHandler = (e: any) => {
+    const LIMIT_SIZE = 5 * 1024 * 1024;
+
+    try {
+      if (e.target.files.length > 0) {
+        let imageFile = e.target.files;
+
+        /* 사이즈 제한 걸리는 경우 */
+        if (LIMIT_SIZE < imageFile[0].size) {
+          /* 이미지 사이즈 줄이기 */
+          const blobURL = window.URL.createObjectURL(imageFile[0]);
+          const MAX_WIDTH = 1000;
+          const MAX_HEIGHT = 1000;
+          const MIME_TYPE = 'image/jpeg';
+          const QUALITY = 0.8;
+          const img = new Image();
+          img.src = blobURL;
+
+          img.onerror = () => {
+            URL.revokeObjectURL(blobURL);
+            alert('이미지 업로드에 실패했습니다');
+            return;
+          };
+
+          img.onload = () => {
+            URL.revokeObjectURL(blobURL);
+            const [formatWidth, formatHeight] = getImageSize(
+              img,
+              MAX_WIDTH,
+              MAX_HEIGHT
+            );
+            const canvas = document.createElement('canvas');
+            canvas.width = formatWidth;
+            canvas.height = formatHeight;
+            const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+            ctx.drawImage(img, 0, 0, formatWidth, formatHeight);
+            canvas.toBlob(
+              (blob: any) => {
+                imageFile = new File([blob], imageFile[0].name, {
+                  type: imageFile[0].type,
+                });
+                getImageFileReader(imageFile);
+              },
+              MIME_TYPE,
+              QUALITY
+            );
+          };
+        } else {
+          getImageFileReader(imageFile[0]);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getImageFileReader = (imageFile: any) => {
+    setWriteMenuReviewObj({
+      ...writeMenuReviewObj,
+      imgFiles: [...writeMenuReviewObj.imgFiles, imageFile],
+    });
+
+    const imageFileReader = new FileReader();
+
+    imageFileReader.onload = (e: any) => {
+      setPreviewImg([...previewImg, e.target.result]);
+    };
+
+    imageFileReader.readAsDataURL(imageFile);
+  };
+
+  const removePreviewImgHandler = (index: number) => {
+    const filterPreviewImg = previewImg.filter((img, idx) => idx !== index);
+    setPreviewImg(filterPreviewImg);
+  };
 
   if (!Object.keys(item).length) {
     return <div>로딩</div>;
@@ -104,17 +205,15 @@ function WriteReviewPage({ id }: any) {
             터치하여 별점을 선택해주세요.
           </TextH6B>
         </RateWrapper>
-        <ReviewTextForm>
-          <TextArea
-            name="name"
-            placeholder="placeholder"
-            minLength={0}
-            maxLength={1000}
-            rows={20}
-            onChange={writeReviewHandler}
-            ref={textAreaRef}
-          />
-        </ReviewTextForm>
+        <TextArea
+          name="reviewArea"
+          placeholder="placeholder"
+          minLength={0}
+          maxLength={1000}
+          rows={20}
+          eventHandler={writeReviewHandler}
+          ref={textAreaRef}
+        />
         <FlexBetween margin="8px 0 0 0">
           <TextB3R color={theme.brandColor}>
             {30 - numberOfReivewContent}자만 더 쓰면 포인트 적립 조건 충족!
@@ -134,7 +233,35 @@ function WriteReviewPage({ id }: any) {
             진행 될 수 있습니다.
           </TextB3R>
         </FlexRow>
-        <UploadInputWrapper>{/* <UploadInput /> */}</UploadInputWrapper>
+        <FlexRow>
+          <UploadInputWrapper>
+            <TextInput
+              width="100%"
+              height="100%"
+              padding="0"
+              inputType="file"
+              accept="image/*"
+              eventHandler={onChangeFileHandler}
+            />
+            <div className="plusBtn">
+              <SVGIcon name="plus18" />
+            </div>
+          </UploadInputWrapper>
+          {previewImg.length > 0 &&
+            previewImg.map((img: string, index: number) => {
+              return (
+                <PreviewImgWrapper key={index}>
+                  <img src={img} />
+                  <div
+                    className="svgWrapper"
+                    onClick={() => removePreviewImgHandler(index)}
+                  >
+                    <SVGIcon name="blackBackgroundCancel" />
+                  </div>
+                </PreviewImgWrapper>
+              );
+            })}
+        </FlexRow>
       </UploadPhotoWrapper>
     </Container>
   );
@@ -159,41 +286,53 @@ const RateWrapper = styled.div`
   }
 `;
 
-const ReviewTextForm = styled.div``;
-
-const TextArea = styled.textarea`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  max-height: 208px;
-  border: 1px solid ${theme.greyScale15};
-  border-radius: 0;
-  box-shadow: 0 0 0;
-  background: #fff;
-  font-size: 12px;
-  line-height: 18px;
-  font-weight: 400;
-  letter-spacing: -0.4px;
-  padding: 16px;
-  -webkit-tap-highlight-color: transparent;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  resize: none;
-  border-radius: 8px;
-  &:focus {
-    border: 1px solid ${theme.greyScale15};
-    outline: 0;
-  }
-  &::-webkit-input-placeholder {
-    color: #999;
-  }
-`;
-
 const UploadPhotoWrapper = styled.div`
   ${homePadding}
 `;
 
-const UploadInputWrapper = styled.div``;
+const UploadInputWrapper = styled.label`
+  position: relative;
+  display: block;
+  width: 72px;
+  height: 72px;
+  background-color: ${theme.greyScale6};
+  border-radius: 8px;
+  margin: 16px 0 48px 0;
+  input {
+    position: absolute;
+    left: 0;
+    top: 0;
+    opacity: 0;
+  }
+
+  .plusBtn {
+    position: absolute;
+    left: 40%;
+    top: 35%;
+  }
+`;
+
+const PreviewImgWrapper = styled.div`
+  position: relative;
+  width: 72px;
+  height: 72px;
+  background-color: ${theme.greyScale6};
+  border-radius: 8px;
+  margin: 16px 0 48px 0;
+  border: none;
+
+  > img {
+    width: 100%;
+    height: 100%;
+  }
+  .svgWrapper {
+    svg {
+      position: absolute;
+      right: 10%;
+      top: 10%;
+    }
+  }
+`;
 
 export async function getServerSideProps(context: any) {
   const { id } = context.query;
