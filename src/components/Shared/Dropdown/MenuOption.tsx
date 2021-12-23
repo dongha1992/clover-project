d063d64f9049f92fae03417dbca9ed233ed4b30e
donghakim <donghakim@donghakimui-MacBookPro.local>
2021-12-23 11:14:11 +0900

    DEV-839 cart bottom sheet 메뉴 선택 UI 변경

--- src/components/Shared/Dropdown/MenuOption.tsx
+++ src/components/Shared/Dropdown/MenuOption.tsx
@@ -1,6 +1,7 @@
 import React, { useState, ReactNode } from 'react';
 import styled from 'styled-components';
-import { TextB3R, TextH6B } from '@components/Shared/Text';
+import { TextB3R, TextH6B, TextH7B } from '@components/Shared/Text';
+import { FlexBetween, FlexRow, theme } from '@styles/theme';
 
 type TProps = {
   // children: ReactNode | ReactNode[];
@@ -12,8 +13,17 @@ type TProps = {
 function MenuOption({ option, selectMenuHandler }: TProps) {
   return (
     <OptionList onClick={() => selectMenuHandler(option)}>
-      <TextB3R>{option.text}</TextB3R>
-      <TextH6B padding="0 0 0 2px">{option.price}원</TextH6B>
+      <FlexBetween padding="0 0 4px 0">
+        <TextB3R>{option.name}</TextB3R>
+        <TextH7B>매일 60개 한정 판매</TextH7B>
+      </FlexBetween>
+      <FlexRow>
+        <TextH6B color={theme.brandColor}>{option.discount}%</TextH6B>
+        <TextH6B padding="0 4px">{option.price}원</TextH6B>
+        <TextH6B color={theme.greyScale65} textDecoration="line-through">
+          {option.price}원
+        </TextH6B>
+      </FlexRow>
     </OptionList>
   );
 }
@@ -22,6 +32,7 @@ export default React.memo(MenuOption);
 
 const OptionList = styled.li`
   display: flex;
+  flex-direction: column;
   list-style-type: none;
   padding: 12px 16px;
   border-bottom: 1px solid #e4e4e4;
