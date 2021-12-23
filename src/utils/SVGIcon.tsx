9efd33affcf412b0f05ebc8854623c1128c1cf9e
donghakim <donghakim@donghakimui-MacBookPro.local>
2021-12-23 15:24:25 +0900

    DEV-839 coupon down 부분 UI추가

--- src/utils/SVGIcon.tsx
+++ src/utils/SVGIcon.tsx
@@ -1518,6 +1518,23 @@ const svgMap: TSvg = {
       />
     </svg>
   ),
+  checkBlack18: () => (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 9L7.75 13L14 5"
+        stroke="#242424"
+        strokeWidth="1.6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
 };
 
 /* TODO: ref */
