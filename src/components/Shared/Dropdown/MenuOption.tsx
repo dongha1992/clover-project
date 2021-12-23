feb139092bbfebe6facb80fbecd80a5bf8be537e
donghakim <donghakim@donghakimui-MacBookPro.local>
2021-12-23 12:35:11 +0900

    DEV-839 카트 아이템 UI 수정

--- src/components/Shared/Dropdown/MenuOption.tsx
+++ src/components/Shared/Dropdown/MenuOption.tsx
@@ -1,7 +1,13 @@
 import React, { useState, ReactNode } from 'react';
 import styled from 'styled-components';
 import { TextB3R, TextH6B, TextH7B } from '@components/Shared/Text';
-import { FlexBetween, FlexRow, theme } from '@styles/theme';
+import {
+  FlexBetween,
+  FlexCol,
+  FlexRow,
+  FlexRowStart,
+  theme,
+} from '@styles/theme';
 
 type TProps = {
   // children: ReactNode | ReactNode[];
@@ -13,10 +19,13 @@ type TProps = {
 function MenuOption({ option, selectMenuHandler }: TProps) {
   return (
     <OptionList onClick={() => selectMenuHandler(option)}>
-      <FlexBetween padding="0 0 4px 0">
+      <FlexBetween>
         <TextB3R>{option.name}</TextB3R>
         <TextH7B>매일 60개 한정 판매</TextH7B>
       </FlexBetween>
+      <FlexRowStart padding="0 0 4px 0">
+        <TextH7B color={theme.brandColor}>(굿바이 세일 1+1 증정)</TextH7B>
+      </FlexRowStart>
       <FlexRow>
         <TextH6B color={theme.brandColor}>{option.discount}%</TextH6B>
         <TextH6B padding="0 4px">{option.price}원</TextH6B>
@@ -33,6 +42,7 @@ export default React.memo(MenuOption);
 const OptionList = styled.li`
   display: flex;
   flex-direction: column;
+
   list-style-type: none;
   padding: 12px 16px;
   border-bottom: 1px solid #e4e4e4;
