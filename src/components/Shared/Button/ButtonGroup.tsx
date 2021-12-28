59faa59157b3150ba8b75f8113e1899062a82008
donghakim <donghakim@donghakimui-MacBookPro.local>
2021-12-28 14:19:58 +0900

    DEV-858 카드 편집 UI 완성

---
+++ src/components/Shared/Button/ButtonGroup.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { fixedBottom, theme } from '@styles/theme';
+import Button from '@components/Shared/Button';
+
+interface IProps {
+  leftButtonHandler: () => void;
+  rightButtonHandler: () => void;
+  leftText: string;
+  rightText: string;
+}
+
+export const ButtonGroup = React.memo(
+  ({ leftButtonHandler, rightButtonHandler, leftText, rightText }: IProps) => {
+    return (
+      <BtnWrapper>
+        <Button
+          height="100%"
+          width="100%"
+          borderRadius="0"
+          onClick={leftButtonHandler}
+        >
+          {leftText}
+        </Button>
+        <Col />
+        <Button
+          height="100%"
+          width="100%"
+          borderRadius="0"
+          onClick={rightButtonHandler}
+        >
+          {rightText}
+        </Button>
+      </BtnWrapper>
+    );
+  }
+);
+
+const BtnWrapper = styled.div`
+  ${fixedBottom}
+  display: flex;
+`;
+
+const Col = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: 25%;
+  background-color: ${theme.white};
+  width: 1px;
+  height: 50%;
+`;
---
+++ src/pages/mypage/card/edit/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import SVGIcon from '@utils/SVGIcon';
+import { FlexCenter, FlexCol, homePadding, FlexRow } from '@styles/theme';
+import BorderLine from '@components/Shared/BorderLine';
+import TextInput from '@components/Shared/TextInput';
+import { TextH5B } from '@components/Shared/Text';
+import { editCard } from '@api/card';
+import { ButtonGroup } from '@components/Shared/Button/ButtonGroup';
+import dynamic from 'next/dynamic';
+
+const Checkbox = dynamic(() => import('@components/Shared/Checkbox'), {
+  ssr: false,
+});
+
+function CardEditPage() {
+  const [isMainCard, setIsMainCard] = useState<boolean>(false);
+
+  const changeCardNameHandler = () => {};
+
+  const removeCard = async () => {};
+
+  const editCard = async () => {};
+
+  return (
+    <Container>
+      <FlexCenter padding="32px 0 0 0">
+        <SVGIcon name="cardRegister" />
+      </FlexCenter>
+      <BorderLine height={1} margin="32px 0" />
+      <FlexCol margin="0 0 24px 0">
+        <TextH5B padding="0 0 9px 0">카드 별명</TextH5B>
+        <TextInput value={'별명'} eventHandler={changeCardNameHandler} />
+      </FlexCol>
+      <FlexRow>
+        <Checkbox
+          isSelected={isMainCard}
+          onChange={() => setIsMainCard(!isMainCard)}
+        />
+        <TextH5B padding="4px 0 0 8px">대표 카드로 설정</TextH5B>
+      </FlexRow>
+      <ButtonGroup
+        leftButtonHandler={removeCard}
+        rightButtonHandler={editCard}
+        leftText="삭제하기"
+        rightText="수정하기"
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  ${homePadding}
+`;
+
+export default CardEditPage;
