097088f10a77fda57a4bcde3c78b83098f5e7666
young <young@youngui-MacBookAir.local>
2022-01-01 19:47:04 +0900

    DEV-860 간편주문 도착날짜 계산

--- src/constants/quick/index.ts
+++ src/constants/quick/index.ts
@@ -5,7 +5,7 @@ export const CARD = {
   },
   lunch2: {
     title: '스팟점심 주문',
-    dec: '9시30분까지 주문 시 n일 12시 전 도착',
+    dec: '9시30분까지 주문 시 %s일 12시 전 도착',
   },
   lunch3: {
     title: '스팟점심 주문',
@@ -18,7 +18,7 @@ export const CARD = {
   },
   dinner2: {
     title: '스팟저녁 주문',
-    dec: '11시까지 주문 시 n일 17시 전 도착',
+    dec: '11시까지 주문 시 %s일 17시 전 도착',
   },
   dinner3: {
     title: '스팟저녁 주문',
@@ -31,7 +31,7 @@ export const CARD = {
   },
   dawn2: {
     title: '새벽배송 주문',
-    dec: '17시까지 주문 시 n일 새벽 7시 전 도착',
+    dec: '17시까지 주문 시 %s일 새벽 7시 전 도착',
   },
   dawn3: {
     title: '새벽배송 주문',
@@ -44,7 +44,7 @@ export const CARD = {
   },
   delivery2: {
     title: '택배배송 주문',
-    dec: '17시까지 주문 시 n일 당일 발송',
+    dec: '17시까지 주문 시 %s일 당일 발송',
   },
   delivery3: {
     title: '택배배송 주문',
@@ -54,9 +54,9 @@ export const CARD = {
 };
 
 export const MENT = {
-  type1:'오늘 점심도 빠르게!',
-  type2:'오늘 저녁도 빠르게!',
-  type3:'내일 아침도 미리!',
-  type4:'내일 점심도 빠르게!',
-  type5:'간편하게 주문해요!',
+  type1: '오늘 점심도 빠르게!',
+  type2: '오늘 저녁도 빠르게!',
+  type3: '내일 아침도 미리!',
+  type4: '내일 점심도 빠르게!',
+  type5: '간편하게 주문해요!',
 }; 
