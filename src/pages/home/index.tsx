1c87fbcedf32d51e3db01a922167adb1160c68e4
donghakim <donghakim@donghakimui-MacBookPro.local>
2022-01-13 18:08:15 +0900

    DEV-926 ssr 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -23,12 +23,10 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    (context): any => {
-      store.dispatch(SET_ORDER_TYPE({ orderType: 'TEST' }));
-      console.log(store.getState());
-    }
-);
+// export const getStaticProps = wrapper.getStaticProps((store) => (context) => {
+//   store.dispatch(SET_ORDER_TYPE({ orderType: 'TEST' }));
+//   console.log(store.getState().order, 'getServerSideProps order');
+//   console.log(store.getState().cart, 'getServerSideProps cart');
+// });
 
 export default HomePage;
