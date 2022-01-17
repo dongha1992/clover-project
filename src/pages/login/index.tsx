f33f47e7896422baada0de59ae5cfd6b6a2f267e
dongha1992 <kamdunga2002@gmail.com>
2022-01-17 17:36:40 +0900

    DEV-858 package 정리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -20,7 +20,6 @@ import {
   SET_LOGIN_SUCCESS,
   SET_TEMP_PASSWORD,
 } from '@store/user';
-import wrapper from '@store/index';
 import { userLogin } from '@api/user';
 import { EMAIL_REGX, PASSWORD_REGX } from '@pages/signup/email-password';
 
