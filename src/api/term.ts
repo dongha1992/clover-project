1cbdedc403e7f2a79845b18b51dbaaa7e16fcc3a
dongha1992 <kamdunga2002@gmail.com>
2022-01-18 12:09:24 +0900

    DEV-931 마크다운 lib 추가

--- src/api/term.ts
+++ src/api/term.ts
@@ -1,2 +1,9 @@
 import { AxiosResponse } from 'axios';
 import { Api } from './Api';
+import { ITermRequest, ITermResponse } from '@model/index';
+
+export const terms = (
+  params: ITermRequest
+): Promise<AxiosResponse<ITermResponse>> => {
+  return Api.get('terms/v1/terms', { params });
+};
--- src/pages/mypage/term/use/index.tsx
+++ src/pages/mypage/term/use/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import TextInput from '@components/Shared/TextInput';
 import SVGIcon from '@utils/SVGIcon';
 import { setBottomSheet } from '@store/bottomSheet';
 import { useDispatch } from 'react-redux';
 import TermSheet from '@components/BottomSheet/TermSheet';
-import { homePadding, theme } from '@styles/theme';
+import { theme } from '@styles/theme';
+import { terms } from '@api/term';
+import { ITerm } from '@model/index';
+import ReactMarkDown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const TermOfUsePage = () => {
-  const DATE = '2021-22-22';
-
+  const [termOfUser, setTermOfUse] = useState<ITerm>();
   const dispatch = useDispatch();
+
+  const getTerms = async () => {
+    const params = {
+      type: 'USE',
+    };
+    const { data } = await terms(params);
+    setTermOfUse(data.data);
+  };
 
   const clickInputHandler = () => {
     dispatch(
@@ -20,15 +31,35 @@ const TermOfUsePage = () => {
       })
     );
   };
+
+  useEffect(() => {
+    getTerms();
+  }, []);
+
+  const formatDate = termOfUser?.terms.createdAt.split(' ')[0];
+  const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+`;
   return (
     <Container>
       <Wrapper>
         <InputWrapper>
-          <TextInput value={DATE} />
+          <TextInput value={formatDate} />
           <div className="svgWrapper" onClick={clickInputHandler}>
             <SVGIcon name="triangleDown" />
           </div>
         </InputWrapper>
+        <ReactMarkDown>{markdown}</ReactMarkDown>
       </Wrapper>
     </Container>
   );
