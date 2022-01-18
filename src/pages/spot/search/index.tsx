d00a468c4802982f679e8e624e6c9476a80e7993
dongha1992 <kamdunga2002@gmail.com>
2022-01-18 18:57:24 +0900

    DEV-931 bottom sheet 개별 적용 완료

--- src/pages/spot/search/index.tsx
+++ src/pages/spot/search/index.tsx
@@ -125,7 +125,6 @@ const SpotSearchPage = (): ReactElement => {
     dispatch(
       setBottomSheet({
         content: <PickupSheet />,
-        buttonTitle: '주문하기',
       })
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
