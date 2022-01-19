10f29c7a0d00ccfacb798cdb86c53d4985dd7dbf
dongha1992 <kamdunga2002@gmail.com>
2022-01-19 14:09:02 +0900

    DEV-934 access method sheet 추가

---
+++ src/constants/payment/index.ts
@@ -0,0 +1,42 @@
+export const ACCESS_METHOD = [
+  {
+    id: 1,
+    text: '자유출입 가능',
+    value: 'FREE',
+  },
+  {
+    id: 2,
+    text: '공동현관 비밀번호',
+    value: 'COMMON_ENTRANCE_PASSWORD',
+  },
+  {
+    id: 3,
+    text: '경비실 호출',
+    value: 'CALL_SECURITY_OFFICE',
+  },
+  {
+    id: 4,
+    text: '세대 호출',
+    value: 'CALL_HOUSE',
+  },
+  {
+    id: 5,
+    text: '경비실 배송',
+    value: 'DELIVERY_SECURITY_OFFICE',
+  },
+  {
+    id: 6,
+    text: '무인택배함 배송 (외부) ',
+    value: 'DELIVERY_EXTERNAL_UNMANNED_COURIER_BOX',
+  },
+  {
+    id: 8,
+    text: '무인택배함 배송 (공동현관 내부)',
+    value: 'DELIVERY_INTERNAL_UNMANNED_COURIER_BOX',
+  },
+  {
+    id: 9,
+    text: '기타',
+    value: 'ETC',
+  },
+];
