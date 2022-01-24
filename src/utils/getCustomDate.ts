7a39c97023c03a6b6477a3e6e4da201ca1d8e548
Tayler <tayler@freshcode.me>
2022-01-24 15:29:28 +0900

    DEV-954 date 함수 추가

--- src/utils/getCustomDate.ts
+++ src/utils/getCustomDate.ts
@@ -46,30 +46,4 @@ const getCustomDate = (inputDate: Date): IResult => {
   };
 };
 
-// export const dayFormatter = (inputDate: Date) => {
-//   const originalDate = new Date(inputDate);
-
-//   let month = `${originalDate.getMonth() + 1}`;
-//   let date = `${originalDate.getDate()}`;
-//   const day = DAYS[originalDate.getDay()];
-
-//   if (month.length === 1) month = `0${month}`;
-//   if (date.length === 1) date = `0${date}`;
-
-//   return `${month}.${date} (${day})`;
-// };
-
-// export const dayFormatterHyphen = (inputDate: Date) => {
-//   const originalDate = new Date(inputDate);
-
-//   const year = `${originalDate.getFullYear()}`;
-//   let month = `${originalDate.getMonth() + 1}`;
-//   let date = `${originalDate.getDate()}`;
-
-//   if (month.length === 1) month = `0${month}`;
-//   if (date.length === 1) date = `0${date}`;
-
-//   return `${year}-${month}-${date}`;
-// };
-
 export default getCustomDate;
