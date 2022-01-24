c09536234fba082c3d190a2e3a1c4947c84c2b7d
Tayler <tayler@freshcode.me>
2022-01-24 14:02:29 +0900

    DEV-952 메뉴 메인 옵션 수정

--- src/constants/mock/index.ts
+++ src/constants/mock/index.ts
@@ -5,7 +5,7 @@ export const SEARCH_RESULT = [
 ];
 
 export const BASE_URL =
-  'https://gist.githubusercontent.com/dongha1992/7780e6a89c3feb8ffab266a8b9e34f12/raw/43aea1e337a5f32ff4c483b53ca52d8d9cbb8d9f/items.json';
+  'https://gist.githubusercontent.com/dongha1992/7780e6a89c3feb8ffab266a8b9e34f12/raw/ffdfaf8b12265c90f11c84437859f089274d1fbf/items.json';
 
 export const REVIEWS_URL = 'https://jsonplaceholder.typicode.com';
 
