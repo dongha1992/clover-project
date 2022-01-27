3be66865516dd0e7153ab95b390c30618350987d
dongha1992 <kamdunga2002@gmail.com>
2022-01-27 18:40:59 +0900

    DEV-954 in progress

--- src/utils/checkTimerLimitHelper.ts
+++ src/utils/checkTimerLimitHelper.ts
@@ -1,39 +1,43 @@
+import { destinationForm } from '@store/destination';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import getCustomDate from './getCustomDate';
 
 const checkTimerLimitHelper = (currentTime: number): string => {
+  const { locationStatus } = useSelector(destinationForm);
   const { days } = getCustomDate(new Date());
-
-  console.log(days);
+  console.log(locationStatus);
   const spotLunch = currentTime >= 9.0 && currentTime < 9.3;
   const spotDinner = currentTime >= 10.3 && currentTime < 11.0;
   const morning = currentTime >= 16.3 && currentTime < 17.0;
   const parcel = currentTime >= 16.3 && currentTime < 17.0;
 
-  //
   const isWeekends = days === '토' || days === '일';
 
-  switch (true) {
-    case spotLunch: {
-      return '스팟점심';
-    }
-    case spotDinner: {
-      return '스팟저녁';
-    }
-    case morning: {
-      return '새벽배송';
-    }
-    case parcel: {
-      return '택배배송';
-    }
+  useEffect(() => {}, []);
+  return '';
+  // switch (true) {
+  //   case spotLunch: {
+  //     return '스팟점심';
+  //   }
+  //   case spotDinner: {
+  //     return '스팟저녁';
+  //   }
+  //   case !isWeekends && morning: {
+  //     return '새벽배송';
+  //   }
+  //   case !isWeekends && parcel: {
+  //     return '택배배송';
+  //   }
 
-    case isWeekends: {
-      return '';
-    }
+  //   case isWeekends: {
+  //     return '';
+  //   }
 
-    default: {
-      return '';
-    }
-  }
+  //   default: {
+  //     return '';
+  //   }
+  // }
 };
 
 export default checkTimerLimitHelper;
