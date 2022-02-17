16a33e24dcf88f7ed92eb22688de404e3b7bfcdd
young <young@freshcode.me>
2022-02-17 16:21:33 +0900

    DEV-979 회원탈퇴 페이지

--- src/api/user.ts
+++ src/api/user.ts
@@ -14,44 +14,37 @@ import {
   IHelpEmail,
   IHelpPassword,
   IChangePassword,
+  ISecessionResponse,
 } from '@model/index';
 
-export const userLogin = (
-  data: ILogin
-): Promise<AxiosResponse<ILoginResponse>> => {
-  return Api.post('user/v1/login', data);
+export const userLogin = (data: ILogin): Promise<AxiosResponse<ILoginResponse>> => {
+  return Api.post('/user/v1/login', data);
 };
 
 export const kakaoLogin = (data: IkakaoLogin): Promise<AxiosResponse<any>> => {
   return Api.post('/user/v1/signin-kakao', data);
 };
 
-export const userAuthTel = (
-  data: IAuthTel
-): Promise<AxiosResponse<IResponse>> => {
+export const userAuthTel = (data: IAuthTel): Promise<AxiosResponse<IResponse>> => {
   return Api.post('/user/v1/auth/tel', data);
 };
 
-export const userConfirmTel = (
-  data: IConfimTel
-): Promise<AxiosResponse<IResponse>> => {
+export const userConfirmTel = (data: IConfimTel): Promise<AxiosResponse<IResponse>> => {
   return Api.post('/user/v1/confirm/tel', data);
 };
 
-export const availabilityEmail = (
-  params: IAavilabiltyEmail
-): Promise<AxiosResponse<IConfirmTelResponse>> => {
+export const availabilityEmail = (params: IAavilabiltyEmail): Promise<AxiosResponse<IConfirmTelResponse>> => {
   return Api.get('/user/v1/availability/email', { params });
 };
-export const signup = (
-  data: ISignupUser
-): Promise<AxiosResponse<ISignupResponse>> => {
+export const signup = (data: ISignupUser): Promise<AxiosResponse<ISignupResponse>> => {
   return Api.post('/user/v1/users', data);
 };
 
-export const userRefreshToken = (
-  refreshToken: string
-): Promise<AxiosResponse<ILoginResponse>> => {
+export const userSecession = <params>(data: params): Promise<AxiosResponse<ISecessionResponse>> => {
+  return Api.delete('/user/v1/users', data);
+};
+
+export const userRefreshToken = (refreshToken: string): Promise<AxiosResponse<ILoginResponse>> => {
   return Api.post('/user/v1/token/refresh', {
     refreshToken: `Bearer ${refreshToken}`,
   });
@@ -61,15 +54,11 @@ export const userProfile = (): Promise<AxiosResponse<any>> => {
   return Api.get('/user/v1/me');
 };
 
-export const userHelpEmail = (
-  data: IHelpEmail
-): Promise<AxiosResponse<IResponse>> => {
+export const userHelpEmail = (data: IHelpEmail): Promise<AxiosResponse<IResponse>> => {
   return Api.post('/user/v1/help/email', data);
 };
 
-export const userHelpPassword = (
-  data: IHelpPassword
-): Promise<AxiosResponse<IResponse>> => {
+export const userHelpPassword = (data: IHelpPassword): Promise<AxiosResponse<IResponse>> => {
   return Api.post('/user/v1/help/password', data);
 };
 
@@ -77,9 +66,7 @@ export const userConfirmPassword = (data: any): Promise<AxiosResponse<any>> => {
   return Api.post('/user/v1/confirm/password', data);
 };
 
-export const userChangePassword = (
-  data: IChangePassword
-): Promise<AxiosResponse<any>> => {
+export const userChangePassword = (data: IChangePassword): Promise<AxiosResponse<any>> => {
   return Api.post('/user/v1/password', data);
 };
 
