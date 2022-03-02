f1065d73830b2c0ae3872a011db0204cc7a64be3
dongha1992 <kamdunga2002@gmail.com>
2022-03-02 18:17:03 +0900

    DEV-990 카테고리 페이지 추가

--- src/components/Pages/Category/SingleMenu.tsx
+++ src/components/Pages/Category/SingleMenu.tsx
@@ -3,11 +3,39 @@ import styled from 'styled-components';
 import { FlexWrapWrapper } from '@styles/theme';
 import { Item } from '@components/Item';
 import { TextH3B } from '@components/Shared/Text';
+import { useQuery } from 'react-query';
+import { getMenusApi } from '@api/menu';
+import { IMenus } from '@model/index';
 
-const SingleMenu = ({ title, data }: any) => {
+const SingleMenu = ({ title }: any) => {
+  const {
+    data,
+    error: menuError,
+    isLoading,
+  } = useQuery<IMenus[]>(
+    'getMenus',
+    async () => {
+      const params = { categories: '', menuSort: 'LAUNCHED_DESC', searchKeyword: '', type: 'SALAD' };
+      const { data } = await getMenusApi(params);
+      return data.data;
+    },
+
+    {
+      onSuccess: (data) => {
+        return data;
+      },
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  if (isLoading) {
+    return <div>로딩중</div>;
+  }
+
   return (
     <Container>
-      <TextH3B padding="0 0 17px 0">{title}</TextH3B>
+      <TextH3B padding="0 0 17px 0">{title || '전체'}</TextH3B>
       <FlexWrapWrapper>
         {data?.map((item: any, index: number) => {
           return <Item item={item} key={index} />;
--- src/pages/category/salad.tsx
+++ src/pages/category/salad.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import { SingleMenu } from '@components/Pages/Category';
 import { categoryPageSet } from '@styles/theme';
 
-const CategoryPage = () => {
+const SaladPage = () => {
   return (
     <Container>
-      <SingleMenu title="전체" />
+      <SingleMenu title="샐러드" />
     </Container>
   );
 };
@@ -15,4 +15,4 @@ const Container = styled.div`
   ${categoryPageSet}
 `;
 
-export default React.memo(CategoryPage);
+export default SaladPage;
