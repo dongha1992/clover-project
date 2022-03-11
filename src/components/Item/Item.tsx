9926a4426983b4654642a8ba387442f6902636e8
Tayler <tayler@freshcode.me>
2022-03-11 12:07:24 +0900

    zindex 수정

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -111,7 +111,7 @@ const ForReopen = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(36, 36, 36, 0.5);
-  z-index: 10;
+  z-index: 9;
   display: flex;
   justify-content: center;
   align-items: center;
