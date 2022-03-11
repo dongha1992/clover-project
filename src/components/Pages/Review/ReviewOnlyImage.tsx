54d694c3b42528396123709407c51a2eb1abc5a5
dongha1992 <kamdunga2002@gmail.com>
2022-03-11 11:57:34 +0900

    DEV-990 wip

--- src/components/Pages/Review/ReviewOnlyImage.tsx
+++ src/components/Pages/Review/ReviewOnlyImage.tsx
@@ -126,7 +126,7 @@ const LastImg = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(36, 36, 36, 0.5);
-  z-index: 10;
+  z-index: 9;
   display: flex;
   justify-content: center;
   align-items: center;
