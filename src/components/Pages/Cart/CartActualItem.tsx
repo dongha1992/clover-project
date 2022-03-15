fddc13937549683115781eb1abb3b4600a207541
dongha1992 <kamdunga2002@gmail.com>
2022-03-15 18:13:07 +0900

    DEV-1025 장바구니 UI 수정

--- src/components/Pages/Cart/CartActualItem.tsx
+++ src/components/Pages/Cart/CartActualItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { TextH5B, TextB3R, TextH6B } from '@components/Shared/Text';
-import { FlexBetween, theme } from '@styles/theme';
+import { FlexBetween, theme, FlexCol, FlexBetweenStart } from '@styles/theme';
 import CountButton from '@components/Shared/Button/CountButton';
 import SVGIcon from '@utils/SVGIcon';
 import { Tag } from '@components/Shared/Tag';
@@ -22,40 +22,42 @@ const CartActualItem = ({
   clickMinusButton,
   clickRestockNoti,
 }: IProps) => {
-  const isSoldout = false;
+  console.log(menu);
   return (
-    <Container isSoldout={isSoldout}>
+    <Container isSoldout={menu.isSoldout}>
       <ContentWrapper>
-        <TextB3R>{menu.name}</TextB3R>
         <FlexBetween>
+          <TextB3R>{menu.name}</TextB3R>
+          <div onClick={() => removeCartItemHandler && removeCartItemHandler(menu.id)}>
+            <SVGIcon name="defaultCancel" />
+          </div>
+        </FlexBetween>
+        <FlexCol>
           <PriceWrapper>
-            <TextH5B color={isSoldout ? theme.greyScale25 : theme.brandColor} padding={'0 4px 0 0'}>
+            <TextH5B color={menu.isSoldout ? theme.greyScale25 : theme.brandColor} padding={'0 4px 0 0'}>
               {menu.discount}%
             </TextH5B>
             <TextH5B>{menu.price}원</TextH5B>
           </PriceWrapper>
-          <RemoveBtnContainer onClick={() => removeCartItemHandler && removeCartItemHandler(menu.id)}>
-            <SVGIcon name="defaultCancel" />
-          </RemoveBtnContainer>
-          <CountButtonContainer>
-            {isSoldout ? (
-              <Tag backgroundColor={theme.black} padding="6px 10px" borderRadius={32} onClick={clickRestockNoti}>
-                <TextH6B color={theme.white}>재입고 알림</TextH6B>
-              </Tag>
-            ) : (
-              <CountButton
-                id={menu.id}
-                quantity={menu.quantity}
-                clickPlusButton={clickPlusButton}
-                clickMinusButton={clickMinusButton}
-              />
-            )}
-          </CountButtonContainer>
-        </FlexBetween>
+          <FlexBetweenStart>
+            <InfoMessage status={menu.isSoldout ? 'soldOut' : 'soldSoon'} count={2} />
+            <CountButtonContainer>
+              {menu.isSoldout ? (
+                <Tag backgroundColor={theme.black} padding="6px 10px" borderRadius={32} onClick={clickRestockNoti}>
+                  <TextH6B color={theme.white}>재입고 알림</TextH6B>
+                </Tag>
+              ) : (
+                <CountButton
+                  id={menu.id}
+                  quantity={menu.quantity}
+                  clickPlusButton={clickPlusButton}
+                  clickMinusButton={clickMinusButton}
+                />
+              )}
+            </CountButtonContainer>
+          </FlexBetweenStart>
+        </FlexCol>
       </ContentWrapper>
-      <div className="itemInfo">
-        <InfoMessage status="soldSoon" count={2} />
-      </div>
     </Container>
   );
 };
@@ -63,12 +65,17 @@ const CartActualItem = ({
 const Container = styled.div<{ isSoldout?: boolean }>`
   display: flex;
   color: ${({ isSoldout }) => isSoldout && theme.greyScale25};
+  background-color: ${theme.greyScale3};
+  padding: 16px;
+  margin-bottom: 8px;
+  border-radius: 8px;
+  position: relative;
 `;
 
 const ContentWrapper = styled.div`
   margin-left: 8px;
   width: 100%;
-  height: 60px;
+  height: 70px;
 `;
 
 const PriceWrapper = styled.div`
@@ -76,14 +83,10 @@ const PriceWrapper = styled.div`
   margin-bottom: 8px;
 `;
 
-const RemoveBtnContainer = styled.div`
-  position: absolute;
-  right: 12px;
-  top: 12px;
-`;
-
 const CountButtonContainer = styled.div`
-  margin-top: 10px;
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
 `;
 
 export default React.memo(CartActualItem);
