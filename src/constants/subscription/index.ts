186d3f356b8c1c7a986530623e3442e3af069e88
young <young@freshcode.me>
2022-04-07 12:47:25 +0900

    DEV-1152 구독하기 UI

---
+++ src/constants/subscription/index.ts
@@ -0,0 +1,27 @@
+export const SUBSCRIPTION_PERIOD = [
+  {
+    id: 1,
+    period: 'oneWeek',
+    text: '1주 (3% 할인)',
+  },
+  {
+    id: 2,
+    period: 'twoWeek',
+    text: '2주 (3% 할인)',
+  },
+  {
+    id: 3,
+    period: 'threeWeek',
+    text: '3주 (5% 할인)',
+  },
+  {
+    id: 4,
+    period: 'fourWeek',
+    text: '4주 (5% 할인)',
+  },
+  {
+    id: 5,
+    period: 'subscription',
+    text: '정기구독 (최대 15% 할인)',
+  },
+];
