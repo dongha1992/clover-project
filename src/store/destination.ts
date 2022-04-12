3f7046276f2037c6f473189d4d4fdae88736198d
dongha1992 <kamdunga2002@gmail.com>
2022-04-12 11:41:02 +0900

    DEV-1342 배송정보 최근 주문 이력 수정

--- src/store/destination.ts
+++ src/store/destination.ts
@@ -1,6 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppState } from '.';
-import { IJuso, IRegisterDestination } from '@model/index';
+import {
+  IJuso,
+  IMenuImage,
+  IOrderMenusInOrderList,
+  IOrderOptions,
+  IRegisterDestination,
+  IGetOrderInImage,
+  IOrderInOrderList,
+} from '@model/index';
 import { TLocationType } from '@utils/checkDestinationHelper';
 import { availabilityDestination } from '@api/destination';
 
@@ -32,6 +40,20 @@ export interface IDestination {
   deliveryTime?: string;
   spotPickupId?: number;
   spotPickup?: any;
+
+  deliveryDate?: string;
+  deliveryDateChangeCount?: number;
+  deliveryDateChangeMaximum?: number;
+  deliveryDetail?: string;
+
+  image?: IGetOrderInImage;
+  order?: IOrderInOrderList;
+  orderMenus?: IOrderMenusInOrderList[];
+  orderOptions?: IOrderOptions[];
+  spotId?: number;
+  spotName?: string;
+  spotPickupName?: string;
+  status?: string;
 }
 
 interface TProps {
