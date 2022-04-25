2099f63c243824d52d8526b7c451b62995e6956b
young <young@freshcode.me>
2022-04-25 11:58:30 +0900

    DEV-1167 구독 상세 UI

--- src/components/Pages/Subscription/detail/SubsDetailDeliveryBox.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const SubsDetailDeliveryBox = () => {
-  return <Container></Container>;
-};
-const Container = styled.div``;
-export default SubsDetailDeliveryBox;
