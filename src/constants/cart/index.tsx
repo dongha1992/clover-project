61e87801e27c9dfa12a46a7aa59e867ff7c333dd
Tayler <tayler@freshcode.me>
2022-04-26 15:43:14 +0900

    DEV-1383 cart 배송비 합산 관련 로직

--- src/constants/cart/index.tsx
+++ src/constants/cart/index.tsx
@@ -1,57 +1,6 @@
-interface IDeliveryObj {
-  spot: {
-    fee: number;
-    amountForFree: number;
-    minimum: number;
-  };
-  quick: {
-    fee: number;
-    amountForFree: number;
-    minimum: number;
-  };
-  morning: {
-    fee: number;
-    amountForFree: number;
-    minimum: number;
-  };
-  parcel: {
-    fee: number;
-    amountForFree: number;
-    minimum: number;
-  };
-  trialSpot?: {
-    fee: number;
-    amountForFree: number;
-    minimum: number;
-  };
-}
+import { Obj } from '@model/index';
 
-// // 배송비
-// deliveryFeeObj: {
-//   spot: 0,
-//   trialSpot: 4000,
-//   quick: 4000,
-//   morning: 3500,
-//   parcel: 3500
-// },
-// // 배송비 무료 금액
-// freeDeliveryAmountObj: {
-//   spot: 0,
-//   trialSpot: 40000,
-//   quick: 40000,
-//   morning: 35000,
-//   parcel: 35000
-// },
-// // 최소주문금액
-// minOrderAmountObj: {
-//   spot: 6000,
-//   trialSpot: 6000,
-//   quick: 10000,
-//   morning: 10000,
-//   parcel: 10000
-// }
-
-export const DELIVERY_FEE_OBJ: IDeliveryObj = {
+export const DELIVERY_FEE_OBJ: Obj = {
   spot: {
     fee: 0,
     amountForFree: 0,
@@ -60,7 +9,7 @@ export const DELIVERY_FEE_OBJ: IDeliveryObj = {
   quick: {
     fee: 4000,
     amountForFree: 40000,
-    minimum: 40000,
+    minimum: 10000,
   },
   morning: {
     fee: 3500,
@@ -73,7 +22,7 @@ export const DELIVERY_FEE_OBJ: IDeliveryObj = {
     minimum: 10000,
   },
   trialSpot: {
-    fee: 400,
+    fee: 4000,
     amountForFree: 40000,
     minimum: 6000,
   },
