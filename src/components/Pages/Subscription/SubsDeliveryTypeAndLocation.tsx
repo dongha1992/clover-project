86661575817ed58d1fb0301f205479bdb847832a
young <young@freshcode.me>
2022-05-03 14:04:18 +0900

    DEV-1152 정기구독 새벽/택배 최근 배송지조회

--- src/components/Pages/Subscription/SubsDeliveryTypeAndLocation.tsx
+++ src/components/Pages/Subscription/SubsDeliveryTypeAndLocation.tsx
@@ -1,6 +1,7 @@
 import { TextB3R, TextH4B } from '@components/Shared/Text';
 import { DELIVERY_TYPE_MAP } from '@constants/order';
 import { ILocation } from '@model/index';
+import { IDestinationAddress } from '@pages/subscription/set-info';
 import { theme } from '@styles/theme';
 import { SVGIcon } from '@utils/common';
 import styled from 'styled-components';
@@ -10,13 +11,13 @@ interface IProps {
   deliveryType?: string;
   deliveryDestination?: ILocation | null;
   spotMainDestination?: string;
+  mainDestinationAddress?: IDestinationAddress;
 }
 const SubsDeliveryTypeAndLocation = ({
   goToDeliveryInfo,
   subsDeliveryType,
-  deliveryType,
-  deliveryDestination,
   spotMainDestination,
+  mainDestinationAddress,
 }: IProps) => {
   return (
     <Container onClick={goToDeliveryInfo}>
@@ -29,10 +30,10 @@ const SubsDeliveryTypeAndLocation = ({
           </TextB3R>
         </Left>
       )}
-      {(subsDeliveryType === 'PARCEL' || subsDeliveryType === 'MORNING') && (
+      {['PARCEL', 'MORNING'].includes(subsDeliveryType) && (
         <Left>
-          <TextH4B>{deliveryType ? DELIVERY_TYPE_MAP[deliveryType.toUpperCase()] : '배송방법'}</TextH4B>
-          <TextH4B>{deliveryDestination ? deliveryDestination?.dong : '배송지를 설정해 주세요'}</TextH4B>
+          <TextH4B>{mainDestinationAddress ? DELIVERY_TYPE_MAP[subsDeliveryType] : '배송방법'}</TextH4B>
+          <TextH4B>{mainDestinationAddress ? mainDestinationAddress.address : '배송지를 설정해 주세요'}</TextH4B>
           <TextB3R color={theme.greyScale65} padding="8px 0 0">
             배송방법이 제한된 상품입니다.
           </TextB3R>
