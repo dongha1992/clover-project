112450b618a429bed891d8df9fd1b8fb4c6e2a06
flynn <flynn@freshcode.me>
2022-05-14 20:58:37 +0900

    PT-317 스팟 현황 - 스팟 오픈 안내 페이지 작업 in progress

--- src/components/Pages/Mypage/Spot/OpenInfo.tsx
+++ src/components/Pages/Mypage/Spot/OpenInfo.tsx
@@ -2,43 +2,89 @@ import React from 'react';
 import styled from 'styled-components';
 import { TextB3R, TextH5B } from '@components/Shared/Text';
 import { theme } from '@styles/theme';
+import { IMAGE_S3_DEV_URL } from '@constants/mock';
+import { SVGIcon } from '@utils/common';
+
+interface IParams {
+  type: string;
+};
 
 const CONTENT_TEXT = [
-    {
-      title: '검토 중',
-      desc: '제출한 신청서를 검토하는 단계.\n트라이얼 진행을 위한 스팟의 배송 적합성을 확인합니다.',
-    },
-    {
-      title: '트라이얼 진행 중',
-      desc: '트라이얼 승인 후 임시 사용 가능한 스팟으로 운영됩니다.\n2주간 5명 이상의 고객이 배송완료하면 정상오픈을 위한 스텝을 진행하며, 신청자와 참여자 모두에게 혜택을 드립니다.',
-    },
-    {
-      title: '오픈 검토 중',
-      desc: '트라이얼 2주가 종료되면 기간동안 운영, 배송에 발생한 이슈를 확인하고 최종 오픈을 위한 검토를 진행합니다.',
-    },
-    {
-      title: '오픈완료',
-      desc: '오픈완료 시점부터 정상적으로 스팟을 사용할 수 있습니다.',
-    },
-    {
-      title: '오픈 미진행',
-      desc: '트라이얼 인원이 부족하거나 배송, 출입 등의 이슈로 운영조건을 충족하지 못한 스팟은 미진행으로 처리됩니다.\n미진행된 동일한 스팟은 최대 2회까지만 재신청이 가능합니다.',
-    },
-    {
-      title: '모집 혜택',
-      desc: '트라이얼 2주간 5명 이상 배송완료하여 오픈 조건을 충족하고 정상 오픈하면 혜택을 드립니다.\n신청자는 본인을 제외한 배송완료 인원당 3,000P(최대 150,000P) 지급, 신청자를 제외한 모든 참여자는 3,000P를 지급받습니다. ',
-    },
-  ];
+        {
+          url: '/img_spot_review.png',
+          title: '1. 제출한 신청서를 검토 중이에요.',
+          desc: '트라이얼 진행을 위해 신청한 장소의 배송 적합성을 확인해요.',
+        },
+        {
+          url: '/img_spot_trial.png',
+          title: '2. 트라이얼 2주간 임시로 사용 가능해요.',
+          desc: '신청한 장소로 트라이얼 2주간 5명 이상의 고객이 배송완료해야 다음 단계로 넘어갈 수 있어요. 신청자와 참여자 모두에게 혜택을 제공하니 배송비 무료로 트라이얼 주문해 보세요! (단, 트라이얼은 구독 주문할 수 없어요.)',
+        },
+        {
+          url: '/img_spot_open_review.png',
+          title: '3. 최종 오픈을 위해 검토 중이에요.',
+          desc: '트라이얼 2주가 종료되면 기간동안 운영, 배송에 발생한 이슈를 확인하고 최종 오픈을 위한 검토를 진행합니다.',
+        },
+        {
+          url: '/img_spot_open.png',
+          title: '4. 프코스팟을 오픈 완료했어요.',
+          desc: '오픈완료 시점부터 정상적으로 프코스팟을 사용할 수 있으니 배송비 무료로 스팟배송을 이용해 보세요!',
+          notice: {
+            title: '프코스팟 오픈 혜택 받아가세요!',
+            desc: ['•신청자는 본인을 제외한 참여자(배송완료) 인원 당 3,000P (최대 150,000P)', '•신청자를 제외한 모든 참여자는 3,000P'],
+            noticeIcon: 'pointGreen18',
+          },
+        },
+        {
+          url: '/img_spot_private_retry.png',
+          icon: 'exclamationMark',
+          title: '아쉽지만 오픈 미진행될 수 있어요.',
+          desc: '트라이얼 모집인원이 부족하거나 배송, 출입 등의 이슈로 운영 조건을 충족하지 못했을 때 미진행으로 처리돼요.',
+          notice: {
+            title: '트라이얼 모집 실패로 오픈 미진행되었다면?',
+            desc: ['오픈 미진행된 시점으로 30일 후 동일한 장소로 트라이얼 재신청할 수 있어요. 재신청 기회는 최대 2회, 모집인원은 0명으로 다시 시작해요!'],
+          }
+        },
+];  
 
-const OpenInfo = () => {
+const OpenInfo = ({type}: IParams) => {
   return (
     <Container>
       {
         CONTENT_TEXT.map((i, idx) => {
           return (
             <ContentTextWrapper key={idx}>
-              <TextH5B margin='0 0 8px 0'>{i.title}</TextH5B>
-              <TextB3R>{i.desc}</TextB3R>
+              <Img src={`${IMAGE_S3_DEV_URL}${i.url}`} />
+              <Wrapper>
+                {
+                  i.icon && (
+                    <SVGIcon name={i?.icon} />
+                  )
+                }
+                <TextH5B margin='0 0 0 4px'>{i.title}</TextH5B>
+              </Wrapper>
+              <TextB3R color={theme.greyScale65}>{i.desc}</TextB3R>
+              {
+                i.notice && (
+                  <NoticeWrapper>
+                    <Contetn>
+                      {
+                        i.notice.noticeIcon && (
+                          <SVGIcon name={i.notice.noticeIcon} />
+                        )
+                      }
+                      <TextH5B margin='0 0 8px 4px' color={theme.brandColor}>{i.notice.title}</TextH5B>
+                    </Contetn>
+                    {
+                      i.notice.desc.map((j, idx) => {
+                        return (
+                          <TextB3R key={idx} color={theme.brandColor}>{j}</TextB3R>
+                        )
+                      })
+                    }
+                  </NoticeWrapper>
+                )
+              }
             </ContentTextWrapper>
           )
         })
@@ -49,11 +95,32 @@ const OpenInfo = () => {
 
 const Container = styled.div`
   padding: 24px;
-  background: ${theme.greyScale3};
+  background: ${theme.white};
 `;
 
 const ContentTextWrapper = styled.div`
-  margin-bottom: 24px;
+  margin-bottom: 32px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  margin: 0 0 8px 0;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  margin: 0 0 16px 0;
+`;
+
+const NoticeWrapper = styled.div`
+  padding: 20px;
+  background: ${theme.brandColor3p};
+  border-radius: 8px;
+  margin-top: 16px;
+`;
+
+const Contetn = styled.div`
+  display: flex;
 `;
 
 export default OpenInfo;
