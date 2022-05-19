38b2ea535dc7c8441809b24090c8d3c5bd27ed07
dongha1992 <kamdunga2002@gmail.com>
2022-05-19 11:54:16 +0900

    PT-635 test

--- src/pages/mypage/term/index.tsx
+++ src/pages/mypage/term/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { MypageMenu } from '../index';
+// import { MypageMenu } from '../index';
 
 /* TODO: 약관 페이지 3개 중복코드 및 date format 부분 리팩토링 */
 
@@ -8,9 +8,9 @@ const TermPage = () => {
   return (
     <Container>
       <Wrapper>
-        <MypageMenu title="이용 약관" link="/mypage/term/use" />
+        {/* <MypageMenu title="이용 약관" link="/mypage/term/use" />
         <MypageMenu title="개인정보 처리방침" link="/mypage/term/privacy" />
-        <MypageMenu title="위치정보 서비스 이용 약관" link="/mypage/term/location" hideBorder />
+        <MypageMenu title="위치정보 서비스 이용 약관" link="/mypage/term/location" hideBorder /> */}
       </Wrapper>
     </Container>
   );
