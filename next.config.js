f39988e6bb0674bc002f46e20133bf332f2cc71c
Tayler <tayler@freshcode.me>
2022-05-23 14:13:03 +0900

    PT-644-404 next-build-id 제거

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextBuildId = require('next-build-id');
-
 // module.exports = {
 //   async rewrites() {
 //     return [
