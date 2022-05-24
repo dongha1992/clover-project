d91e384a356737ed929a66ba92852e794eeb8103
flynn <flynn@freshcode.me>
2022-05-24 17:29:33 +0900

    PT-645 console warnging 수정, css 수정

--- src/components/Pages/Mypage/Spot/SpotStatusList.tsx
+++ src/components/Pages/Mypage/Spot/SpotStatusList.tsx
@@ -120,53 +120,50 @@ const SpotStatusList = ({ items, onClick }: IProps): ReactElement => {
     <Container>
       {items?.map((i, idx) => {
         return (
-          <>
-            <Wrppaer key={idx}>
-              <FlexBetween margin="0 0 6px 0">
-                <Flex>
-                  <TextH4B color={`${i?.rejected ? theme.greyScale65 : theme.black}`} margin="0 8px 0 0">
-                    {spotStatusStep(i)}
-                  </TextH4B>
-                  <Tag color={theme.brandColor} backgroundColor={theme.brandColor5P}>
-                    {spotType(i?.type)}
-                  </Tag>
-                </Flex>
-                <TextH6B color={theme.greyScale65} textDecoration="underline" pointer onClick={() => onClick(i?.id)}>
-                  신청상세 보기
-                </TextH6B>
-              </FlexBetween>
-              <TextH5B>{i?.placeName}</TextH5B>
-              <TextB3R>{`${i?.location.address} ${i?.location.addressDetail}`}</TextB3R>
-              {
-                (i.type === 'PRIVATE' && (i.step === 'TRIAL' || (i?.trialUserCount! >= i?.trialTargetUserCount!))) && !i?.rejected &&
-                // 프라이빗인 경우 '트라이얼' or '오픈 검토중' 노출
-                <FlexStart margin="4px 0 0 0">
-                  <SVGIcon name="people" />
-                  <TextH6B padding='4px 0 0 0' margin="0 0 0 6px" color={theme.brandColor}>{`${i?.trialUserCount}/5명 참여 중`}</TextH6B>
-                </FlexStart>
-              }
-              {
-                (i.type === 'PUBLIC' && i.step === 'RECRUITING') &&
-                // 단골가게(퍼블릭)인 경우 '모집 중' 노출
-                <FlexStart margin="4px 0 0 0">
-                  <SVGIcon name="people" />
-                  <TextH6B padding='4px 0 0 0' margin="0 0 0 6px" color={theme.brandColor}>{`${i?.recruitingCount}/100명 참여 중`}</TextH6B>
-                </FlexStart>
-              }
-              {i?.type === 'PRIVATE' && i?.step === 'TRIAL' && !i?.rejected && !i?.canRetrial && (
-                <Button border color={theme.black} backgroundColor={theme.white} margin="16px 0 0 0">
-                  오픈 참여 공유하고 포인트 받기
-                </Button>
-              )}
-              {
-                i?.type === 'PRIVATE' && i?.step === 'TRIAL' && i?.canRetrial &&
-                <Button border color={theme.black} backgroundColor={theme.white} margin="16px 0 0 0" onClick={() => handleSpotRetrial(i?.id!)}>
-                  오픈 재신청하기
-                </Button>
-              }
-            </Wrppaer>
-            <Row />
-          </>
+          <Wrppaer key={idx}>
+            <FlexBetween margin="0 0 6px 0">
+              <Flex>
+                <TextH4B color={`${i?.rejected ? theme.greyScale65 : theme.black}`} margin="0 8px 0 0">
+                  {spotStatusStep(i)}
+                </TextH4B>
+                <Tag color={theme.brandColor} backgroundColor={theme.brandColor5P}>
+                  {spotType(i?.type)}
+                </Tag>
+              </Flex>
+              <TextH6B color={theme.greyScale65} textDecoration="underline" pointer onClick={() => onClick(i?.id)}>
+                신청상세 보기
+              </TextH6B>
+            </FlexBetween>
+            <TextH5B>{i?.placeName}</TextH5B>
+            <TextB3R>{`${i?.location.address} ${i?.location.addressDetail}`}</TextB3R>
+            {
+              (i.type === 'PRIVATE' && (i.step === 'TRIAL' || (i?.trialUserCount! >= i?.trialTargetUserCount!))) && !i?.rejected &&
+              // 프라이빗인 경우 '트라이얼' or '오픈 검토중' 노출
+              <FlexStart margin="4px 0 0 0">
+                <SVGIcon name="people" />
+                <TextH6B padding='4px 0 0 0' margin="0 0 0 6px" color={theme.brandColor}>{`${i?.trialUserCount}/5명 참여 중`}</TextH6B>
+              </FlexStart>
+            }
+            {
+              (i.type === 'PUBLIC' && i.step === 'RECRUITING') &&
+              // 단골가게(퍼블릭)인 경우 '모집 중' 노출
+              <FlexStart margin="4px 0 0 0">
+                <SVGIcon name="people" />
+                <TextH6B padding='4px 0 0 0' margin="0 0 0 6px" color={theme.brandColor}>{`${i?.recruitingCount}/100명 참여 중`}</TextH6B>
+              </FlexStart>
+            }
+            {i?.type === 'PRIVATE' && i?.step === 'TRIAL' && !i?.rejected && !i?.canRetrial && (
+              <Button border color={theme.black} backgroundColor={theme.white} margin="16px 0 0 0">
+                오픈 참여 공유하고 포인트 받기
+              </Button>
+            )}
+            {
+              i?.type === 'PRIVATE' && i?.step === 'TRIAL' && i?.canRetrial &&
+              <Button border color={theme.black} backgroundColor={theme.white} margin="16px 0 0 0" onClick={() => handleSpotRetrial(i?.id!)}>
+                오픈 재신청하기
+              </Button>
+            }
+          </Wrppaer>
         );
       })}
     </Container>
@@ -177,19 +174,17 @@ const Container = styled.div`
   padding-top: 70px;
 `;
 
-const Wrppaer = styled.div``;
+const Wrppaer = styled.div`
+  padding: 24px 0;
+  border-bottom: 1px solid ${theme.greyScale6};
+  &: last-child {
+    border: none;
+  }
+`;
 
 const Flex = styled.div`
   display: flex;
   align-items: center;
-`;
-
-const Row = styled.div`
-  border-bottom: 1px solid ${theme.greyScale6};
-  margin: 24px 0;
-  &: last-child {
-    border: none;
-  }
 `;
 
 export default SpotStatusList;
