1c2da9b97367a0c57b2f68d7c7ebc82114f84c57
dongha1992 <kamdunga2002@gmail.com>
2022-05-30 16:38:14 +0900

    PT-652 나이스페이/계좌이체 연동

--- src/constants/login/index.tsx
+++ src/constants/login/index.tsx
@@ -1,12 +1,12 @@
 export const FIND_ACCOUNT = [
   {
     text: '이메일',
-    link: '/login/find-account/email',
+    link: '/login/find-account/email/',
     value: 'email',
   },
   {
     text: '비밀번호',
-    link: '/login/find-account/password',
+    link: '/login/find-account/password/',
     value: 'password',
   },
 ];
