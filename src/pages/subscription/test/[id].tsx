78177fd1c174ee3a99a968d709ec95e3e1aae849
young <young@freshcode.me>
2022-06-02 17:19:15 +0900

    PT-344 빌드에러 수정

--- src/pages/subscription/test/[id].tsx
+++ src/pages/subscription/test/[id].tsx
@@ -1,30 +1,31 @@
-import axios from 'axios';
-import { GetStaticPropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const TestPage = ({ menuDetail }: { menuDetail: any }) => {
-  return (
-    <div>
-      <h1>getStaticProps test page</h1>
-      <p>메뉴 이름 : {menuDetail.name}</p>
-      <p>메뉴 내용 : {menuDetail.description}</p>
-    </div>
-  );
+const TestPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState<string | string[] | undefined>();
+  useEffect(() => {
+    if (router.isReady) {
+      setId(router.query.id);
+    }
+  }, []);
+  return <div>{id}</div>;
 };
-export const getStaticProps = async (context: GetStaticPropsContext) => {
-  const id = context.params?.id as string;
-  const { data } = await axios(`${process.env.API_URL}/menu/v1/menus/${id}`);
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const id = context.params?.id as string;
+//   const { data } = await axios(`${process.env.API_URL}/menu/v1/menus/${id}`);
 
-  if (!id) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: true,
-      },
-    };
-  }
+//   if (!id) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: true,
+//       },
+//     };
+//   }
 
-  return {
-    props: { menuDetail: data.data },
-  };
-};
+//   return {
+//     props: { menuDetail: data.data },
+//   };
+// };
 export default TestPage;
