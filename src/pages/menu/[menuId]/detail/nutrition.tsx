453e775690abe902dce040a5bdaba3e094bff7ab
Tayler <tayler@freshcode.me>
2022-06-18 00:36:02 +0900

    PT-722 상세 정보 페이지 완성

--- src/pages/menu/[menuId]/detail/nutrition.tsx
+++ src/pages/menu/[menuId]/detail/nutrition.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { IProductInfo } from '@model/index';
+import { useSelector } from 'react-redux';
+import { menuSelector } from '@store/menu';
+import { TextH5B } from '@components/Shared/Text';
 
 const NutritionInfoPage = () => {
-  return <div>로딩</div>;
+  const { info } = useSelector(menuSelector);
+
+  return (
+    <Container>
+      <Table>
+        <TextH5B padding="0 0 18px 0"></TextH5B>
+      </Table>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 24px;
+`;
+
+const Table = styled.div``;
 
 export default NutritionInfoPage;
--- src/pages/menu/[menuId]/detail/product.tsx
+++ src/pages/menu/[menuId]/detail/product.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { IProductInfo } from '@model/index';
+import { useSelector } from 'react-redux';
+import { menuSelector } from '@store/menu';
+import { TextH5B } from '@components/Shared/Text';
 
 const ProductInfoPage = () => {
-  return <div>로딩</div>;
+  const { menuItem } = useSelector(menuSelector);
+
+  return (
+    <Container>
+      <Table>
+        <TextH5B padding="0 0 18px 0">{menuItem.productInfoNotis.businessName}</TextH5B>
+      </Table>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 24px;
+`;
+
+const Table = styled.div``;
 
 export default ProductInfoPage;
