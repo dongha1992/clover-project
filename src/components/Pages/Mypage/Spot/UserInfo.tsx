54cdeccceef8e95a6aa06407a21602c5e194b720
flynn <flynn@freshcode.me>
2022-06-17 15:50:19 +0900

    PT-317 마이페이지 - 스팟 현황 수정요청사항 반영

--- src/components/Pages/Mypage/Spot/UserInfo.tsx
+++ src/components/Pages/Mypage/Spot/UserInfo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { TextB3R, TextH5B } from '@components/Shared/Text';
+import { TextB3R, TextH5B, TextB2R } from '@components/Shared/Text';
 import { theme } from '@styles/theme';
 import { IGetRegistrationStatus } from '@model/index';
 
@@ -12,16 +12,16 @@ const UserInfo = ({ item }: IParams) => {
   return (
     <Container>
       <TextH5B margin='0 0 8px 0'>이름</TextH5B>
-      <TextB3R margin='0 0 24px 0'>{item?.userName}</TextB3R>
+      <TextB2R margin='0 0 24px 0'>{item?.userName}</TextB2R>
       <TextH5B margin='0 0 8px 0'>이메일</TextH5B>
-      <TextB3R margin='0 0 24px 0'>{item?.userEmail}</TextB3R>
+      <TextB2R margin='0 0 24px 0'>{item?.userEmail}</TextB2R>
       <TextH5B margin='0 0 8px 0'>휴대폰 번호</TextH5B>
-      <TextB3R>{item?.userTel}</TextB3R>
+      <TextB2R>{item?.userTel}</TextB2R>
       {
         item?.type === 'OWNER' && (
           <>
             <TextH5B margin='24px 0 8px 0'>직급/호칭</TextH5B>
-            <TextB3R>{item?.userPosition}</TextB3R>
+            <TextB2R>{item?.userPosition}</TextB2R>
           </>
         )
       }
