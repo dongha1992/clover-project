6020a980ed7615e81f92e327b9abbcb4c4504ce3
young <young@freshcode.me>
2022-06-20 02:01:30 +0900

    PT-350 구독관리상세페이지 작성,구독관리상세 구독정보추가

--- src/components/Pages/Subscription/detail/SubsDetailOrderBox.tsx
+++ src/components/Pages/Subscription/detail/SubsDetailOrderBox.tsx
@@ -2,19 +2,32 @@ import { SubsDeliveryChangeSheet } from '@components/BottomSheet/SubsSheet';
 import { Button } from '@components/Shared/Button';
 import SlideToggle from '@components/Shared/SlideToggle';
 import { TextB2R, TextB3R, TextH4B, TextH5B } from '@components/Shared/Text';
+import { IMAGE_S3_URL } from '@constants/mock';
+import { DELIVERY_TIME_MAP, DELIVERY_TYPE_MAP } from '@constants/order';
+import useOrderPrice from '@hooks/subscription/useOrderPrice';
 import { MenuImgBox, MenuLi, MenuTextBox, MenuUl } from '@pages/subscription/register';
 import { SET_BOTTOM_SHEET } from '@store/bottomSheet';
-import { FlexBetween, FlexBetweenStart, FlexColEnd } from '@styles/theme';
-import { SVGIcon } from '@utils/common';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { subscriptionForm } from '@store/subscription';
+import { FlexBetween, FlexBetweenStart, FlexColEnd, theme } from '@styles/theme';
+import { getFormatDate, getFormatPrice, SVGIcon } from '@utils/common';
+import axios from 'axios';
+import { cloneDeep } from 'lodash-es';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { MenuPriceContainer, MenuPriceLi, MenuPriceUl } from '../payment/MenusPriceBox';
+import MenuPriceBox from '../payment/MenuPriceBox';
 
-const SubsDetailOrderBox = () => {
+interface IProps {
+  item: any;
+  subscriptionPeriod: string;
+}
+
+const SubsDetailOrderBox = ({ item, subscriptionPeriod }: IProps) => {
   const [toggleState, setToggleState] = useState(false);
-
   const dispatch = useDispatch();
+
+  const priceInfo = useOrderPrice(item.orderMenus, item.orderOptions);
 
   // TODO : 구독상품 타입에 따라서 주문완료,주문취소,배송완료 변경
   const toggleClickHandler = () => {
@@ -24,87 +37,141 @@ const SubsDetailOrderBox = () => {
   const deliveryDateChangeHandler = () => {
     dispatch(
       SET_BOTTOM_SHEET({
-        content: <SubsDeliveryChangeSheet />,
+        content: <SubsDeliveryChangeSheet item={item} setToggleState={setToggleState} />,
       })
     );
   };
+
   return (
-    <Container className="test">
-      <FlexBetween padding="24px 0" className="box" onClick={toggleClickHandler}>
-        <TextH4B>배송 1회차 - 1월 20일 (목) 도착예정</TextH4B>
+    <Container>
+      <FlexBetween padding="24px 24px" className="box" onClick={toggleClickHandler}>
+        <TextH4B color={`${item?.status === 'CANCELED' && '#C8C8C8'}`}>
+          배송 {item?.deliveryRound ?? 1}회차 - {getFormatDate(item.deliveryDate)}{' '}
+          {item.status === 'COMPLETED' ? '도착' : '도착예정'}{' '}
+          {item?.deliveryDateChangeCount > 0 && <span className="deliveryChange">(배송일변경)</span>}
+        </TextH4B>
         <div className={`toggleIcon ${toggleState ? 'down' : ''}`}>
           <SVGIcon name="triangleDown" />
         </div>
       </FlexBetween>
-      <SlideToggle state={toggleState} duration={0.5}>
+      <SlideToggle state={toggleState} duration={0.5} change={item}>
         <MenuUl className="menuWrpper">
-          {[1, 2, 3].map((item, index) => (
+          {item.orderMenus.map((menu: any, index: number) => (
             <MenuLi key={index} className="menuLi">
-              <MenuImgBox></MenuImgBox>
+              <MenuImgBox>
+                <Image
+                  src={IMAGE_S3_URL + menu.image.url}
+                  alt="상품이미지"
+                  width={'100%'}
+                  height={'100%'}
+                  layout="responsive"
+                />
+              </MenuImgBox>
               <MenuTextBox>
-                <TextB3R textHideMultiline>
-                  훈제오리 애플시나몬 샐러드 / 미디움 (M)훈제오리 애플시나몬 샐러드 / 미디움 (M)훈제오리 애플시나몬
-                  샐러드 / 미디움 (M)훈제오리 애플시나몬 샐러드 / 미디움 (M)훈제오리 애플시나몬 샐러드 / 미디움 (M)
+                <TextB3R textHideMultiline color={`${item?.status === 'CANCELED' && '#C8C8C8'}`}>
+                  {menu.menuName} / {menu.menuDetailName}
                 </TextB3R>
                 <div className="wrap">
-                  <TextH5B>2,610원</TextH5B>
+                  <TextH5B color={`${item?.status === 'CANCELED' && '#C8C8C8'}`}>
+                    {getFormatPrice(String(menu.menuPrice - menu.menuDiscount))}원
+                  </TextH5B>
                   <div className="line"></div>
-                  <TextB2R>1개</TextB2R>
+                  <TextB2R color={`${item?.status === 'CANCELED' && '#C8C8C8'}`}>1개</TextB2R>
                 </div>
               </MenuTextBox>
             </MenuLi>
           ))}
         </MenuUl>
-        <MenuPriceContainer>
-          <MenuPriceUl>
-            <MenuPriceLi>
-              <TextB2R>상품금액</TextB2R>
-              <TextB2R>9,700원</TextB2R>
-            </MenuPriceLi>
-            <MenuPriceLi>
-              <TextB2R>배송비</TextB2R>
-              <TextB2R>0원</TextB2R>
-            </MenuPriceLi>
-            <MenuPriceLi>
-              <TextB2R>포크+물티슈</TextB2R>
-              <TextB2R>1개 / 100원</TextB2R>
-            </MenuPriceLi>
-            <MenuPriceLi>
-              <TextB2R>젓가락+물티슈</TextB2R>
-              <TextB2R>1개 / 100원</TextB2R>
-            </MenuPriceLi>
-          </MenuPriceUl>
-          <FlexBetween padding="16px 0 0" className="btB">
-            <TextH4B>배송상품금액</TextH4B>
-            <TextH4B>13,200원</TextH4B>
-          </FlexBetween>
-        </MenuPriceContainer>
+
+        <MenuPriceBox
+          menuPrice={priceInfo.menuPrice}
+          menuDiscount={priceInfo.menuDiscount}
+          eventDiscount={0}
+          menuOption1={priceInfo.option1}
+          menuOption2={priceInfo.option2}
+          deliveryPrice={priceInfo.deliveryPrice}
+          deliveryType={item.delivery}
+          disposable={true}
+        />
+
         <DeliveryInfoBox>
           <TextH4B padding="0 0 24px 0">배송정보</TextH4B>
           <FlexBetween padding="0 0 16px">
             <TextH5B>받는 사람</TextH5B>
-            <TextB2R>김프코</TextB2R>
+            <TextB2R>{item.receiverName}</TextB2R>
           </FlexBetween>
           <FlexBetween padding="0 0 16px">
             <TextH5B>휴대폰 번호</TextH5B>
-            <TextB2R>010-2222-2222</TextB2R>
+            <TextB2R>{item.receiverTel.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)}</TextB2R>
           </FlexBetween>
-          <FlexBetween padding="0 0 16px">
-            <TextH5B>배송방법</TextH5B>
-            <TextB2R>스팟배송 - 점심</TextB2R>
-          </FlexBetween>
-          <FlexBetweenStart padding="0 0 24px">
-            <TextH5B>픽업장소</TextH5B>
-            <FlexColEnd>
-              <TextB2R>헤이그라운드 서웊숲점</TextB2R>
-              <TextB3R color="#717171">서울 성동구 왕십리로 115 10층</TextB3R>
-            </FlexColEnd>
-          </FlexBetweenStart>
+          {item.delivery === 'SPOT' ? (
+            <>
+              <FlexBetween padding="0 0 16px">
+                <TextH5B>배송방법</TextH5B>
+                <TextB2R>
+                  {DELIVERY_TYPE_MAP[item.delivery]} - {DELIVERY_TIME_MAP[item.deliveryDetail]}
+                </TextB2R>
+              </FlexBetween>
+              <FlexBetweenStart padding="0 0 24px">
+                <TextH5B>픽업장소</TextH5B>
+                <FlexColEnd>
+                  <TextB2R>{item.spotName}</TextB2R>
+                  <TextB3R color="#717171">
+                    {item.location.address}
+                    {item.location.addressDetail}
+                  </TextB3R>
+                </FlexColEnd>
+              </FlexBetweenStart>
+            </>
+          ) : (
+            <>
+              <FlexBetween padding="0 0 16px">
+                <TextH5B>배송방법</TextH5B>
+                <TextB2R>{DELIVERY_TYPE_MAP[item.delivery]}</TextB2R>
+              </FlexBetween>
+              <FlexBetweenStart padding="0 0 24px">
+                <TextH5B>픽업장소</TextH5B>
+                <FlexColEnd>
+                  <TextB2R>{item.spotName}</TextB2R>
+                  <TextB2R className="textRight">
+                    {item.location.address} <br />
+                    {item.location.addressDetail}
+                  </TextB2R>
+                </FlexColEnd>
+              </FlexBetweenStart>
+            </>
+          )}
           <FlexBetween>
-            <Button width="48%" backgroundColor="#fff" color="#242424" border>
+            <Button
+              width="48%"
+              backgroundColor="#fff"
+              color="#242424"
+              border
+              disabled={
+                subscriptionPeriod !== 'UNLIMITED' || item?.status === 'COMPLETED' || item?.status === 'CANCELED'
+                  ? true
+                  : false
+              }
+            >
               배송정보 변경하기
             </Button>
-            <Button onClick={deliveryDateChangeHandler} width="48%" backgroundColor="#fff" color="#242424" border>
+            <Button
+              onClick={() => {
+                subscriptionPeriod === 'UNLIMITED' &&
+                  item?.status !== 'COMPLETED' &&
+                  item?.status !== 'CANCELED' &&
+                  deliveryDateChangeHandler();
+              }}
+              width="48%"
+              backgroundColor="#fff"
+              color="#242424"
+              border
+              disabled={
+                subscriptionPeriod !== 'UNLIMITED' || item?.status === 'COMPLETED' || item?.status === 'CANCELED'
+                  ? true
+                  : false
+              }
+            >
               배송일 변경하기
             </Button>
           </FlexBetween>
@@ -136,6 +203,13 @@ const Container = styled.div`
       }
       border: none;
     }
+  }
+  .textRight {
+    text-align: right;
+  }
+  .deliveryChange {
+    font-size: 12px;
+    color: ${theme.brandColor};
   }
 `;
 const DeliveryInfoBox = styled.div`
