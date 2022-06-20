3ec6fa92413befcf3be8aa0cb1c4ccd33aa17ebb
Tayler <tayler@freshcode.me>
2022-06-20 18:28:25 +0900

    PT-729 토글 수정

--- src/components/Pages/Detail/DetailBottomFAQ.tsx
+++ src/components/Pages/Detail/DetailBottomFAQ.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { TextB2R, TextH4B, TextB3R, TextH6B } from '@components/Shared/Text';
 import SlideToggle from '@components/Shared/SlideToggle';
@@ -8,6 +8,7 @@ import { IMenuFaq } from '@model/index';
 interface IContents {
   description: string;
   title: string;
+  isToggle: boolean;
 }
 
 interface IProps {
@@ -15,20 +16,32 @@ interface IProps {
 }
 
 const DetailBottomFAQ = ({ menuFaq }: IProps) => {
-  const [toggleObj, setToggleObj] = useState('');
+  const [faq, setFaq] = useState<IContents[]>();
 
   const setToggleHandler = (title: string) => {
-    setToggleObj(title);
+    const found = faq?.map((item) => {
+      if (item.title === title) {
+        return { ...item, isToggle: !item.isToggle };
+      }
+      return { ...item, isToggle: false };
+    });
+    setFaq(found);
   };
+
+  useEffect(() => {
+    const editFaq = menuFaq?.contents?.map((item) => {
+      return { ...item, isToggle: false };
+    });
+    setFaq(editFaq);
+  }, [menuFaq]);
 
   return (
     <Container>
-      {menuFaq?.contents?.map((content: IContents, index: number) => {
-        const isToggle = toggleObj === content.title;
+      {faq?.map((content: IContents, index: number) => {
         return (
           <Contents key={index}>
-            <ToggleHeader setIsToggle={setToggleHandler} text={content.title} isToggle={isToggle} />
-            <SlideToggle state={isToggle} duration={0.5}>
+            <ToggleHeader setIsToggle={setToggleHandler} text={content.title} isToggle={content.isToggle} />
+            <SlideToggle state={content.isToggle} duration={0.5}>
               <Content>
                 <TextH6B padding="0 0 12px 0">{content.title}</TextH6B>
                 <TextB3R>{content.description}</TextB3R>
@@ -42,7 +55,7 @@ const DetailBottomFAQ = ({ menuFaq }: IProps) => {
 };
 
 const Container = styled.div`
-  padding: 8px 24px 24px 24px;
+  padding: 24px;
 `;
 
 const Content = styled.div`
@@ -55,9 +68,6 @@ const Content = styled.div`
 `;
 
 const Contents = styled.div`
-  border-bottom: 1px solid ${theme.greyScale6};
-  &:last-of-type {
-    border-bottom: none;
-  }
+  margin-top: 24px;
 `;
 export default DetailBottomFAQ;
