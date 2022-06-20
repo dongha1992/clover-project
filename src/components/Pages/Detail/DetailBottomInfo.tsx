df3bf660117cb8120e405bb24dc1991afb67b746
dongha1992 <kamdunga2002@gmail.com>
2022-06-20 18:43:11 +0900

    PT-729 상세 정보 마크업 추가

--- src/components/Pages/Detail/DetailBottomInfo.tsx
+++ src/components/Pages/Detail/DetailBottomInfo.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { TextB2R } from '@components/Shared/Text';
-import ReactMarkdown from 'react-markdown';
 
-import remarkGfm from 'remark-gfm';
 interface IProps {
   menuDescription?: string;
 }
 
 const DetailBottomInfo = ({ menuDescription }: IProps) => {
-  return (
-    <Container>
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]} // strikethrough, tables, tasklists and URLs를 지원하기 위한 plugin
-        skipHtml={true} // 기본값으로 true가 설정되어있다. markdown source안에서 html태그를 무시하지 않도록 false로 option변경
-      >
-        {menuDescription!}
-      </ReactMarkdown>
-    </Container>
-  );
+  return <Container dangerouslySetInnerHTML={{ __html: menuDescription! }}></Container>;
 };
 
 const Container = styled.div`
