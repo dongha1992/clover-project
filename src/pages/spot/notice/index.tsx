d4d80ca4b1f6301ba2f58ad1e0b7bd70a70097fd
flynn <flynn@freshcode.me>
2022-06-22 12:15:16 +0900

    PT-746 스팟 콘텐츠 가안 이미지 삽입 작업

--- src/pages/spot/notice/index.tsx
+++ src/pages/spot/notice/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { IMAGE_S3_DEV_URL } from '@constants/mock';
 
 const SpotNoticePage = () => {
   return (
     <Container>
-        프코스팟 안내 페이지 
+      <Img src={`${IMAGE_S3_DEV_URL}/img_detail_fco_info.png`} />
     </Container>
   )
 };
 
 const Container = styled.main`
   width: 100%;
-  padding: 24px;
+  height: 100%;
+`;
 
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
 `;
 
 export default SpotNoticePage;
