15a6b8e37432e1b74409dd2d917f12785da93d62
young <young@freshcode.me>
2022-06-27 12:37:37 +0900

    PT-353 react-query hook useGetOrders 추가

--- src/queries/order/index.ts
+++ src/queries/order/index.ts
@@ -1,5 +1,16 @@
-import { getOrderDetailApi } from '@api/order';
+import { getOrderDetailApi, getOrdersApi } from '@api/order';
+import { IGetOrderListRequest } from '@model/index';
 import { QueryKey, useQuery, UseQueryOptions } from 'react-query';
+
+export const useGetOrders = (key: QueryKey, params: IGetOrderListRequest, options?: UseQueryOptions<any>) =>
+  useQuery(
+    key,
+    async () => {
+      const { data } = await getOrdersApi(params);
+      return data.data;
+    },
+    options
+  );
 
 export const useGetOrderDetail = (key: QueryKey, id: number, options?: UseQueryOptions<any>) =>
   useQuery(
