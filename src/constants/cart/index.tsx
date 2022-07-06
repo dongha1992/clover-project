f630105fd5a829319ecba00dc6ddc4d5713d73fb
dongha1992 <kamdunga2002@gmail.com>
2022-07-06 14:15:10 +0900

    PT-778 장바구니 상수 파일 수정

--- src/constants/cart/index.tsx
+++ src/constants/cart/index.tsx
@@ -27,3 +27,29 @@ export const DELIVERY_FEE_OBJ: Obj = {
     minimum: 6000,
   },
 };
+
+export const INITIAL_NUTRITION = {
+  protein: 0,
+  calorie: 0,
+};
+
+export const INITIAL_DELIVERY_DETAIL = [
+  {
+    id: 1,
+    value: 'LUNCH',
+    text: '점심',
+    discription: '(오전 9:30까지 주문시 12:00 전 도착)',
+    isDisabled: false,
+    isSelected: true,
+    time: '12시',
+  },
+  {
+    id: 2,
+    value: 'DINNER',
+    text: '저녁',
+    discription: '(오전 11:00까지 주문시 17:00 전 도착)',
+    isDisabled: false,
+    isSelected: false,
+    time: '17시',
+  },
+];
