5ad10ac8cd62c5667e378e6bea8617df509b6b5b
Tayler <tayler@freshcode.me>
2022-07-08 16:03:57 +0900

    PT-235 상수파일 컴포넌트 분리

--- src/components/Pages/Mypage/Review/index.tsx
+++ src/components/Pages/Mypage/Review/index.tsx
@@ -1,5 +1,6 @@
 import WillWriteReviewItem from './WillWriteReviewItem';
 import CompleteReviewItem from './CompleteReviewItem';
 import ReviewInfo from './ReviewInfo';
+import ReviewInfoBottom from './ReviewInfoBottom';
 
-export { WillWriteReviewItem, ReviewInfo, CompleteReviewItem };
+export { WillWriteReviewItem, ReviewInfo, CompleteReviewItem, ReviewInfoBottom };
