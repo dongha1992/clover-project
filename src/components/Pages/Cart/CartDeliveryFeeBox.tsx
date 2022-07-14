9c70d3b87f7ac00b1b4f674850212a418c800bc8
Tayler <tayler@freshcode.me>
2022-07-14 11:24:31 +0900

    PT-792 배송안내 바텀시트

--- src/components/Pages/Cart/CartDeliveryFeeBox.tsx
+++ src/components/Pages/Cart/CartDeliveryFeeBox.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { TextB2R, TextH6B, TextH5B } from '@components/Shared/Text';
-import { FlexBetween, theme } from '@styles/theme';
-import { getFormatPrice } from '@utils/common';
+import { FlexBetween, FlexRow, theme } from '@styles/theme';
+import { getFormatPrice, SVGIcon } from '@utils/common';
+import { SET_BOTTOM_SHEET } from '@store/bottomSheet';
 
 interface IProps {
   deliveryFee: number;
   deliveryFeeDiscount?: number;
   isLogin: boolean;
+  onClick: () => void;
 }
 
-const CartDeliveryFeeBox = ({ deliveryFee, deliveryFeeDiscount, isLogin }: IProps) => {
+const CartDeliveryFeeBox = ({ deliveryFee, deliveryFeeDiscount, isLogin, onClick }: IProps) => {
   return (
     <>
       <FlexBetween>
-        <TextH5B>배송비</TextH5B>
+        {isLogin ? (
+          <FlexRow>
+            <TextH5B margin="0 2px 0 0">배송비</TextH5B>
+            <div onClick={onClick}>
+              <SVGIcon name="questionMark" />
+            </div>
+          </FlexRow>
+        ) : (
+          <TextH5B>배송비</TextH5B>
+        )}
         {!isLogin ? (
           <TextB2R>{deliveryFee ? `${getFormatPrice(String(deliveryFee))}원` : '무료배송'}</TextB2R>
         ) : (
-          <TextH6B color={theme.brandColor}>배송방법에 따라 배송비가 부과돼요.</TextH6B>
+          <FlexRow>
+            <TextH6B color={theme.brandColor}>배송방법에 따라 배송비가 부과돼요.</TextH6B>
+          </FlexRow>
         )}
       </FlexBetween>
       {deliveryFeeDiscount !== 0 && (
