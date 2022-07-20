38dc05adb4ff680bec04a497566301d91519e536
dongha1992 <kamdunga2002@gmail.com>
2022-07-20 15:57:16 +0900

    PT-821 배송지명 수정

--- src/components/Pages/Cart/DeliveryTypeAndLocation.tsx
+++ src/components/Pages/Cart/DeliveryTypeAndLocation.tsx
@@ -4,20 +4,36 @@ import { TextH4B } from '@components/Shared/Text';
 import { SVGIcon } from '@utils/common';
 import { isNil, isEqual } from 'lodash-es';
 import { DELIVERY_TYPE_MAP } from '@constants/order';
-import { ILocation, IGetOrderList } from '@model/index';
+import { ILocation, IGetOrderList, IDeliveryObj } from '@model/index';
 
 interface IProps {
   goToDeliveryInfo: () => void;
   deliveryType?: string;
-  deliveryDestination?: ILocation | null;
+  deliveryDestination?: IDeliveryObj | null;
 }
 
 const DeliveryTypeAndLocation = ({ goToDeliveryInfo, deliveryType, deliveryDestination }: IProps) => {
+  const isSpot = deliveryType === 'spot';
+  const isNotSpot = ['parcel', 'morning', 'quick'].includes(deliveryType!);
+
+  const renderDestinationInfo = (): string => {
+    switch (true) {
+      case isSpot: {
+        return `${deliveryDestination?.name}`;
+      }
+
+      case isNotSpot: {
+        return `${deliveryDestination?.location?.address} ${deliveryDestination?.location?.addressDetail} `;
+      }
+      default:
+        return '배송지를 설정해 주세요';
+    }
+  };
   return (
     <Container onClick={goToDeliveryInfo}>
       <Left>
         <TextH4B>{deliveryType ? DELIVERY_TYPE_MAP[deliveryType.toUpperCase()] : '배송방법과'}</TextH4B>
-        <TextH4B>{deliveryDestination ? deliveryDestination?.dong : '배송지를 설정해 주세요'}</TextH4B>
+        <TextH4B>{renderDestinationInfo()}</TextH4B>
       </Left>
       <Right>
         <SVGIcon name="arrowRight" />
