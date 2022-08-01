d2821cf3e437e30ceb0a79cc0a6ec7b225fe609f
Tayler <tayler@freshcode.me>
2022-08-01 12:16:25 +0900

    PT-877 수정 사항 완료

--- src/pages/mypage/order-detail/[id]/index.tsx
+++ src/pages/mypage/order-detail/[id]/index.tsx
@@ -34,7 +34,7 @@ import { AxiosError } from 'axios';
 
 const disabledDates: any = [];
 
-/* TODO: delvieryId의 경우 orderDeliveris[0].id 사용 */
+/* delvieryId의 경우 orderDeliveris[0].id 사용 */
 /* 단건의 경우 배열 요소 하나 하지만 정기구독은 배열형태임 */
 
 const OrderDetailPage = () => {
