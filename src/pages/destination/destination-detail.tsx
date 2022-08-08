19750e5b0bc9655aa7ed48538652ef81b1c6483d
Tayler <tayler@freshcode.me>
2022-08-08 14:11:04 +0900

    PT-15 배송지 입력 필수로 수정

--- src/pages/destination/destination-detail.tsx
+++ src/pages/destination/destination-detail.tsx
@@ -24,6 +24,7 @@ import {
 import { SET_TEMP_EDIT_DESTINATION } from '@store/mypage';
 import { checkDestinationHelper } from '@utils/destination';
 import { Obj } from '@model/index';
+import { SET_ALERT } from '@store/alert';
 
 /* TODO: receiverName, receiverTel  */
 
@@ -80,6 +81,16 @@ const DestinationDetailPage = () => {
       return;
     }
 
+    if (!destinationDetailRef?.current?.value) {
+      dispatch(SET_ALERT({ alertMessage: '상세 주소를 입력해주세요.' }));
+      return;
+    }
+
+    if (!destinationNameRef?.current?.value) {
+      dispatch(SET_ALERT({ alertMessage: '배송지명을 입력해주세요.' }));
+      return;
+    }
+
     if (destinationDetailRef.current && destinationNameRef.current) {
       const addressDetail = destinationDetailRef.current.value.toString();
       const name = destinationNameRef.current.value.toString();
@@ -94,6 +105,8 @@ const DestinationDetailPage = () => {
         },
         main: !hasDefaultDestination ? true : isDefaultDestination,
       };
+
+      console.log(hasDefaultDestination, 'hasDefaultDestination');
       if (orderId) {
         dispatch(SET_TEMP_EDIT_DESTINATION(userDestinationInfo));
         dispatch(INIT_DESTINATION_TYPE());
@@ -127,10 +140,7 @@ const DestinationDetailPage = () => {
     try {
       const { data } = await getMainDestinationsApi(params);
       if (data.code === 200) {
-        console.log(data.data, 'asdasdasdasdas');
-        if (!data.data) {
-          setHasDefaultDestination(false);
-        }
+        !data.data ? setHasDefaultDestination(false) : setHasDefaultDestination(true);
       }
     } catch (error) {
       console.error(error);
