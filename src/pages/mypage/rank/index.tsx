8a098e8aee89d9bb92cb3c5daff5e43ef2a73398
dongha1992 <kamdunga2002@gmail.com>
2022-08-11 17:10:01 +0900

    PT-873 회원등급 페이지 수정

--- src/pages/mypage/rank/index.tsx
+++ src/pages/mypage/rank/index.tsx
@@ -11,7 +11,8 @@ import { onUnauthorized } from '@api/Api';
 
 const RankPage = () => {
   const { me, isLoginSuccess } = useSelector(userForm);
-
+  //temp
+  let isMax = false;
   return (
     <Container>
       {isLoginSuccess ? (
@@ -27,18 +28,33 @@ const RankPage = () => {
             <UserRankInfo title="적립금" count={me?.grade.benefit.accumulationRate} id={1} />
           </FlexCol>
           <BorderLine height={1} margin="24px 0" />
-          <FlexCol padding="0 0 48px 0">
-            <FlexRow>
-              <TextB2R padding="0 4px 0 0">다음 등급</TextB2R>
-              <TextH5B>프코팡팡</TextH5B>
-              <TextB2R>까지 남은 금액</TextB2R>
-            </FlexRow>
-            <TextH4B padding="4px 0 16px 0">12333원</TextH4B>
-            <ProgressBar />
-            <TextB3R padding="8px 0 0 0" color={theme.greyScale65}>
-              (기준 결제금액에 할인, 포인트, 취소 금액은 제외됩니다.)
-            </TextB3R>
-          </FlexCol>
+          {isMax ? (
+            <FlexCol padding="0 0 48px 0">
+              <FlexRow>
+                <TextH5B>프코팡팡</TextH5B>
+                <TextB2R padding="0 4px 0 0">으로 남아주실 거죠?</TextB2R>
+              </FlexRow>
+              <TextB3R padding="8px 0 0 0" color={theme.greyScale65}>
+                (기준 결제금액에 할인, 포인트, 취소 금액은 제외됩니다.)
+              </TextB3R>
+            </FlexCol>
+          ) : (
+            <FlexCol padding="0 0 48px 0">
+              <FlexRow>
+                <TextB2R padding="0 4px 0 0">다음 달 예상 등급은</TextB2R>
+                <TextH5B>프코팡팡</TextH5B>,
+              </FlexRow>
+              <FlexRow>
+                <TextH4B color={theme.brandColor}>12333원</TextH4B>
+                <TextB2R padding="0 0 0 4px">더 구매하면 </TextB2R>
+                <TextH5B>프코팡팡</TextH5B>
+                <TextB2R>이 돼요</TextB2R>
+              </FlexRow>
+              <TextB3R padding="8px 0 0 0" color={theme.greyScale65}>
+                (기준 결제금액에 할인, 포인트, 취소 금액은 제외됩니다.)
+              </TextB3R>
+            </FlexCol>
+          )}
         </Wrapper>
       ) : (
         <Wrapper>
@@ -54,7 +70,7 @@ const RankPage = () => {
               margin="16px 0 0 0"
               pointer
             >
-              등급 혜택 받으러 가기
+              로그인 후 등급 혜택 받기
             </TextH5B>
           </FlexCol>
         </Wrapper>
@@ -69,31 +85,10 @@ const RankPage = () => {
         </Wrapper>
       </BrandColor5>
       <PaddingWrapper>
-        <RankInfo
-          name="신규회원"
-          desc="전월 구매내역이 없는 고객"
-          benefit1="적립금 1%"
-          benefit2="신규회원혜택 사용 가능"
-        />
-        <RankInfo
-          name="프코팡"
-          desc="전월 10만원 미만 구매고객"
-          benefit1="적립금 2%"
-          benefit2="5% 할인쿠폰 (2만원 이상 구매 시)"
-        />
-        <RankInfo
-          name="프코팡팡"
-          desc="전월 10만원 이상 30만원 미만 구매고객"
-          benefit1="적립금 2%"
-          benefit2="7% 할인쿠폰 (2만원 이상 구매 시)"
-        />
-        <RankInfo
-          name="프코팡팡팡"
-          desc="전월 30만원 이상 구매고객"
-          benefit1="적립금 3%"
-          benefit2="10% 할인쿠폰 (2만원 이상 구매 시)"
-          isLast
-        />
+        <RankInfo name="신규회원" desc="전월 구매내역이 없는 고객" benefit2="신규회원혜택 사용 가능" />
+        <RankInfo name="프코팡" desc="전월 5만원 미만 구매고객" benefit1="적립금 0.5%" />
+        <RankInfo name="프코팡팡" desc="전월 5만원 이상 20만원 미만 구매고객" benefit1="적립금 1%" />
+        <RankInfo name="프코팡팡팡" desc="전월 20만원 이상 구매고객" benefit1="적립금 1.5%" isLast />
       </PaddingWrapper>
       <PleaseCheck>
         <TextH5B color={theme.greyScale65} padding="24px 0 0 0">
@@ -163,14 +158,18 @@ export const RankInfo = ({ name, desc, benefit1, benefit2, isLast }: any) => {
         {desc}
       </TextB3R>
       <FlexCol padding="14px 0 0 0">
-        <FlexRow>
-          <Dot />
-          <TextB2R>{benefit1}</TextB2R>
-        </FlexRow>
-        <FlexRow>
-          <Dot />
-          <TextB2R>{benefit2}</TextB2R>
-        </FlexRow>
+        {benefit1 && (
+          <FlexRow>
+            <Dot />
+            <TextB2R>{benefit1}</TextB2R>
+          </FlexRow>
+        )}
+        {benefit2 && (
+          <FlexRow>
+            <Dot />
+            <TextB2R>{benefit2}</TextB2R>
+          </FlexRow>
+        )}
       </FlexCol>
       {!isLast && <BorderLine height={1} margin="24px 0" />}
     </FlexCol>
