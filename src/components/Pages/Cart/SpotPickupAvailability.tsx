364e4a6ab33aa2db643f1794014eeac1546261a2
flynn <flynn@freshcode.me>
2022-08-31 19:05:56 +0900

    PT-1002 보관함 유효성 체크 수정, 유효성 여부에 따라 장바구니 분기처리, 결재페이지 팝업 추가

--- src/components/Pages/Cart/SpotPickupAvailability.tsx
+++ src/components/Pages/Cart/SpotPickupAvailability.tsx
@@ -1,75 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { TextH5B, TextB3R } from '@components/Shared/Text';
-import { FlexCol, theme, FlexRow , homePadding} from '@styles/theme';
-import { getPickupAvailabilityApi } from '@api/spot';
+import { TextB3R } from '@components/Shared/Text';
+import { theme, homePadding} from '@styles/theme';
 import  SVGIcon  from '@utils/common/SVGIcon';
-import { useMutation, useQueryClient } from 'react-query';
-import { useDispatch, useSelector } from 'react-redux';
-import { useQuery } from 'react-query';
-import {Spinner} from '@utils/common';
 
 interface IProps {
-  userDeliveryType: string;
-  setIsCheckedPickupAvailability: any;
-  pickupId: any
+  isLoadingPickup: boolean;
+  pickUpAvailability: boolean | undefined;
 }
 const SpotPickupAvailability = ({
-  userDeliveryType, 
-  setIsCheckedPickupAvailability, 
-  pickupId,
+  isLoadingPickup,
+  pickUpAvailability,
 }: IProps) => {
-  const [isAvailability, setIsAvailability] = useState<boolean>(false);
-
-  const {
-    data: pickUpAvailability,
-    error,
-    isLoading,
-  } = useQuery(
-    'getPickupAvailability',
-    async () => {
-      const { data } = await getPickupAvailabilityApi(pickupId);
-      return data.data.isAvailability;
-    },
-    {
-      onSuccess: (data) => {
-        console.log('data', data);
-      },
-      refetchOnMount: true,
-      refetchOnWindowFocus: false,
-      enabled: !!pickupId,
-    }
-  );
-
-  console.log('isLoading', isLoading, 'pickUpAvailability', pickUpAvailability);
-
 
   const renderSpotPickupAvailability = () => {
-    
+    switch(true){
+      case isLoadingPickup: 
+        return (
+          <>
+            {/* 로딩 들어갈 자리 */}
+            <TextB3R color={theme.brandColor}>사용 가능한 보관함을 확인 중이에요.</TextB3R>
+          </>
+        )
+      case pickUpAvailability:
+        return <TextB3R color={theme.greyScale65}>사용 가능한 보관함을 확인했어요. 빠르게 주문해보세요!</TextB3R>
+      case !pickUpAvailability:
+        return (
+          <>
+            <SVGIcon name='exclamationMark' />
+            <TextB3R padding='1px 0 0 4px'  color={theme.brandColor}>현재 사용 가능한 보관함이 없어요.</TextB3R>  
+          </>
+        )
+      default:
+        return <TextB3R color={theme.greyScale65}>사용 가능한 보관함을 확인했어요. 빠르게 주문해보세요!</TextB3R>
+    };
   };
 
   return (
     <Container>
-      {
-        isLoading && (
-          <PickupWrapper>
-            <Spinner />
-            <TextB3R padding='8px 0 0 0' color={theme.greyScale65}>사용 가능한 보관함을 확인 중이에요.</TextB3R>
-          </PickupWrapper>
-        )
-      }
-      {
-        pickUpAvailability ? (
-          <PickupWrapper>
-            <TextB3R padding='8px 0 0 0' color={theme.greyScale65}>사용 가능한 보관함을 확인했어요. 빠르게 주문해보세요!</TextB3R>
-          </PickupWrapper>
-        ) : (
-          <PickupWrapper>
-            <SVGIcon name='exclamationMark' />
-            <TextB3R padding='1px 0 0 4px'  color={theme.brandColor}>현재 사용 가능한 보관함이 없어요.</TextB3R>
-          </PickupWrapper>
-        )
-      }
+      <PickupWrapper>
+        {renderSpotPickupAvailability()}
+      </PickupWrapper>
     </Container>
   );
 };
