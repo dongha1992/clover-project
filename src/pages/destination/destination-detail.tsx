c898156a8625b205527aa8a9f01440d9f2eb9651
dongha1992 <kamdunga2002@gmail.com>
2022-09-01 13:56:47 +0900

    PT-1021 주문 상세 배송지 변경 라우터 수정

--- src/pages/destination/destination-detail.tsx
+++ src/pages/destination/destination-detail.tsx
@@ -115,7 +115,7 @@ const DestinationDetailPage = () => {
         deliveryType = deliveryType as string;
         const reqBody = {
           name,
-          delivery: deliveryType?.toLowerCase(),
+          delivery: deliveryType,
           deliveryMessage: '',
           main: false,
           receiverName: '',
@@ -202,7 +202,15 @@ const DestinationDetailPage = () => {
   };
 
   const goToSearch = () => {
-    orderId ? router.replace(`/destination/search?orderId=${orderId}`) : router.replace('/destination/search');
+    orderId
+      ? router.replace({
+          pathname: `/destination/search`,
+          query: { orderId, deliveryType },
+        })
+      : router.replace({
+          pathname: '/destination/search',
+          query: { deliveryType },
+        });
   };
 
   const goToHome = () => {
