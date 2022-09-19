e98b4bcbbf6c396584a4021f558b99b70ee7d43d
young <young@freshcode.me>
2022-09-19 15:04:24 +0900

    PT-1013 회원정보수정 비밀번호 입력 숫자 제한 수정

--- src/utils/common/goExternalLink.ts
+++ src/utils/common/goExternalLink.ts
@@ -1,42 +1,40 @@
 const goExternalLink = (url: string) => {
   const isCheckedApp = window.navigator.userAgent.includes('fco-clover-webview') ? true : false;
+
   if (!url) return false;
   const userAgent = window.navigator.userAgent;
-  if (
-    isCheckedApp &&
-    window.ReactNativeWebView &&
-    !userAgent.match('.*Android.*')
-  ) {
+
+  if (isCheckedApp && window.ReactNativeWebView && !userAgent.match('fco-clover-webview-android')) {
     window.ReactNativeWebView.postMessage(
       JSON.stringify({
         cmd: 'webview-open-link',
         data: {
-          url
-        }
+          url,
+        },
       })
     );
     return;
-  };
+  }
 
   // 기종 따라 분기
   try {
     if (/windows phone/i.test(userAgent)) {
-    window.open(url, '_blank');
-    return 'Windows Phone';
+      window.open(url, '_blank');
+      return 'Windows Phone';
     }
     //https://stackoverflow.com/questions/15193359/does-android-support-window-location-replace-or-any-equivalent
     if (/android/i.test(userAgent)) {
-    window.open(url, '_blank');
-    return 'Android';
+      window.open(url, '_blank');
+      return 'Android';
     }
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
     if (/iPad|iPhone|iPod/.test(userAgent)) {
-    window.location.href = url;
-    return 'iOS';
+      window.location.href = url;
+      return 'iOS';
     }
   } catch (error) {
     console.error(error);
-  };
+  }
   window.open(url, '_blank');
 };
 
